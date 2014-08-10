869136c6e280accd0ffc16d5a79320f4ca0b0635
Alejandro Forero Cuervo <alefore@gmail.com>
2014-08-10 18:26:53 +0200

    Add LazyString::ToString.

--- lazy_string.h
+++ lazy_string.h
@@ -8,11 +8,21 @@
 namespace afc {
 namespace editor {
 
+using std::string;
+
 class LazyString {
  public:
   virtual ~LazyString() {}
   virtual char get(size_t pos) const = 0;
   virtual size_t size() const = 0;
+
+  string ToString() {
+    string output(size(), 0);
+    for (int i = 0; i < output.size(); i++) {
+      output.at(i) = get(i);
+    }
+    return output;
+  }
 
   bool operator<(const LazyString& x) {
     for (int current = 0; current < size(); current++) {
