6f027fa247329f9f1cd0efe7e42c10d87696e9ef
Alejandro Forero Cuervo <alefore@gmail.com>
2014-08-10 18:43:31 +0200

    Import noop-command implementation.

---
+++ noop_command.cc
@@ -0,0 +1,30 @@
+#include "noop_command.h"
+
+#include "command.h"
+#include "editor.h"
+
+namespace {
+
+using namespace afc::editor;
+
+class NoopCommandImpl : public Command {
+  const string Description() {
+    return "does nothing";
+  }
+
+  void ProcessInput(int c, EditorState* editor_state) {
+  }
+};
+
+}  // namespace
+
+namespace afc {
+namespace editor {
+
+Command* NoopCommand() {
+  static auto output = new NoopCommandImpl();
+  return output;
+}
+
+}  // namespace editor
+}  // namespace afc
---
+++ noop_command.h
@@ -0,0 +1,14 @@
+#ifndef __AFC_EDITOR_NOOP_COMMAND_H__
+#define __AFC_EDITOR_NOOP_COMMAND_H__
+
+namespace afc {
+namespace editor {
+
+class Command;
+
+Command* NoopCommand();
+
+}  // namespace editor
+}  // namespace afc
+
+#endif
