eb35b2712d21b66f96d5b6e767a75f5b4d0ec771
Alejandro Forero Cuervo <alefore@gmail.com>
2014-08-12 09:59:30 +0200

    Add NewMoveableCharBuffer, to be used in a follow up.

--- char_buffer.cc
+++ char_buffer.cc
@@ -9,27 +9,42 @@ namespace editor {
 
 using std::string;
 
-class CharBuffer : public LazyString {
+class MoveableCharBuffer : public LazyString {
  public:
-  CharBuffer(const char* buffer, size_t size) : buffer_(buffer), size_(size) {}
+  MoveableCharBuffer(const char** buffer, size_t size)
+      : buffer_(buffer), size_(size) {}
 
   char get(size_t pos) const {
     assert(pos < size_);
-    return buffer_[pos];
+    return (*buffer_)[pos];
   }
   size_t size() const { return size_; }
 
  protected:
-  const char* buffer_;
+  const char** buffer_;
   size_t size_;
+};
+
+class CharBuffer : public MoveableCharBuffer {
+ public:
+  CharBuffer(const char* buffer, size_t size)
+      : MoveableCharBuffer(&location_, size),
+        location_(buffer) {}
+
+ protected:
+  const char* location_;
 };
 
 class CharBufferWithOwnership : public CharBuffer {
  public:
   CharBufferWithOwnership(const char* buffer, size_t size)
       : CharBuffer(buffer, size) {}
-  ~CharBufferWithOwnership() { free(const_cast<char*>(buffer_)); }
+  ~CharBufferWithOwnership() { free(const_cast<char*>(location_)); }
 };
+
+unique_ptr<LazyString> NewMoveableCharBuffer(const char** buffer, size_t size) {
+  return unique_ptr<LazyString>(new MoveableCharBuffer(buffer, size));
+}
 
 unique_ptr<LazyString> NewCharBuffer(const char* buffer, size_t size) {
   return unique_ptr<LazyString>(new CharBuffer(buffer, size));
