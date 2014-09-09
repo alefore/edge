6f9c8aaa1c0f1f78060e67a35231eb4c8f3e13e3
Alejandro Forero Cuervo <alefore@gmail.com>
2014-09-09 11:30:55 +0200

    Start adding hooks for editing git COMMIT_EDITMSG files.

--- rc/hooks/buffer-reload.cc
+++ rc/hooks/buffer-reload.cc
@@ -12,10 +12,26 @@ string ProcessCCInputLine(string line) {
   return line;
 }
 
+string Basename(string path) {
+  int last_slash = path.find_last_of("/", path.size());
+  if (last_slash == -1) {
+    return path;
+  }
+  return path.substr(last_slash + 1, path.size() - (last_slash + 1));
+}
+
+buffer.set_editor_commands_path("~/.edge/editor_commands/");
+
 string extension = path.substr(dot + 1, path.size() - dot - 1);
+string basename = Basename(path);
 if (extension == "cc" || extension == "h") {
-  buffer.set_editor_commands_path("~/.edge/editor_commands/");
   buffer.set_line_prefix_characters(" /");
   //buffer.set_input_line_processor(ProcessCCInputLine);
   SetStatus("Loaded C file (" + extension + ")");
+  return;
+}
+
+if (basename == "COMMIT_EDITMSG") {
+  buffer.set_line_prefix_characters(" #");
+  SetStatus("GIT commit msg");
 }
