78f34491095feed49846a11a1e9477d4ef551998
Alejandro Forero Cuervo <alefore@gmail.com>
2014-09-15 02:09:45 +0200

    Run bash in its own PTS automatically.

--- rc/hooks/buffer-reload.cc
+++ rc/hooks/buffer-reload.cc
@@ -1,6 +1,3 @@
-string path = buffer.path();
-
-int dot = path.find_last_of(".", path.size());
 
 string ProcessCCInputLine(string line) {
   SetStatus("Got: " + line);
@@ -17,22 +14,33 @@ string Basename(string path) {
 
 buffer.set_editor_commands_path("~/.edge/editor_commands/");
 
-string extension = dot == -1 ? "" : path.substr(dot + 1, path.size() - dot - 1);
-string basename = Basename(path);
-if (extension == "cc" || extension == "h") {
-  buffer.set_line_prefix_characters(" /");
-  //buffer.set_input_line_processor(ProcessCCInputLine);
-  SetStatus("Loaded C file (" + extension + ")");
-  return;
-}
+string path = buffer.path();
+if (path == "") {
+  string command = buffer.command();
+  int space = command.find_first_of(" ", 0);
+  string basecommand = space == -1 ? command : command.substr(0, space);
+  SetStatus("[" + basecommand + "]");
+  if (basecommand == "bash") {
+    buffer.set_pts(true);
+  }
+} else {
+  int dot = path.find_last_of(".", path.size());
+  string extension = dot == -1 ? "" : path.substr(dot + 1, path.size() - dot - 1);
+  string basename = Basename(path);
+  if (extension == "cc" || extension == "h") {
+    buffer.set_line_prefix_characters(" /");
+    SetStatus("Loaded C file (" + extension + ")");
+    return;
+  }
 
-if (basename == "COMMIT_EDITMSG") {
-  buffer.set_line_prefix_characters(" #");
-  SetStatus("GIT commit msg");
-}
+  if (basename == "COMMIT_EDITMSG") {
+    buffer.set_line_prefix_characters(" #");
+    SetStatus("GIT commit msg");
+  }
 
-if (extension == "py") {
-  buffer.set_editor_commands_path("~/.edge/editor_commands/");
-  buffer.set_line_prefix_characters(" #");
-  SetStatus("Loaded Python file (" + extension + ")");
+  if (extension == "py") {
+    buffer.set_editor_commands_path("~/.edge/editor_commands/");
+    buffer.set_line_prefix_characters(" #");
+    SetStatus("Loaded Python file (" + extension + ")");
+  }
 }
