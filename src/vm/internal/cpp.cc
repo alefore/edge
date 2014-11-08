5f347f0dc0399f070fe3262066832027bf4dd5c9
Alejandro Forero Cuervo <alefore@gmail.com>
2014-11-08 23:39:01 +0100

    Erase old file.

--- src/vm/internal/cpp.cc
+++
@@ -1,9 +0,0 @@
-#include "compilation.h"
-#include "while_expression.h"
-#include "if_expression.h"
-#include "constant_expression.h"
-#include "binary_operator.h"
-#include "../public/value.h"
-
-extern "C" {
-#include "cpp.c"
