05c46faa274365c66c4091d573f9e82e5f640c74
Alejandro Forero Cuervo <alefore@gmail.com>
2015-10-17 18:14:24 +0200

    Get rid of unnecessary ToByteString invocation.

--- src/modifiers.cc
+++ src/modifiers.cc
@@ -6,8 +6,7 @@ namespace afc {
 namespace editor {
 
 std::ostream& operator<<(std::ostream& os, const BufferPosition& bp) {
-  // TODO: Get rid of ToByteString invocation below.
-  os << "[" << ToByteString(bp.buffer_name) << ":" << bp.position << "]";
+  os << "[" << bp.buffer_name << ":" << bp.position << "]";
   return os;
 }
 
