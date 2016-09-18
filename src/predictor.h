1e1934f6b8263cde447ad5f72e994de38e9dbffe
Alejandro Forero Cuervo <alefore@gmail.com>
2016-09-18 22:30:05 +0200

    More documentation about predictors.

--- src/predictor.h
+++ src/predictor.h
@@ -16,6 +16,15 @@ using std::vector;
 class EditorState;
 class OpenBuffer;
 
+// A function that generates predictions (autocompletions) for a given prompt
+// input and writes them to a buffer.
+//
+// The buffer will begin with nothing but an empty line. As such, you probably
+// don't want to use OpenBuffer::AppendLine (because that would leave an empty
+// line at the beggining) but, instead, OpenBuffer::AppendToLastLine, followed
+// by OpenBuffer::AppendRawLine(..., empty_line). In other words, once the
+// predictor is done running, the buffer must have an empty line at the end (and
+// not at the beginning).
 typedef function<void(EditorState*, const wstring&, OpenBuffer*)> Predictor;
 
 shared_ptr<OpenBuffer> PredictionsBuffer(
