81e6627d6a4316885423ace4443b31f63e0a1d17
Alejandro Forero Cuervo <alefore@gmail.com>
2017-04-18 13:07:11 +0200

    Import more tests.

---
+++ src/test/buffer_contents_test.cc
@@ -0,0 +1,44 @@
+#include "src/test/line_test.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <unordered_set>
+
+#include "src/buffer_contents.h"
+#include "src/char_buffer.h"
+#include "src/wstring.h"
+
+namespace afc {
+namespace editor {
+namespace testing {
+namespace {
+void TestBufferContentsSnapshot() {
+  BufferContents contents;
+  for (auto& s : { L"alejandro", L"forero", L"cuervo" }) {
+    contents.push_back(std::make_shared<Line>(Line::Options(NewCopyString(s))));
+  }
+  auto copy = contents.copy();
+  CHECK_EQ("alejandro\nforero\ncuervo",
+           ToByteString(contents.ToString()));
+  CHECK_EQ("alejandro\nforero\ncuervo",
+           ToByteString(copy->ToString()));
+
+  contents.SplitLine(1, 3);
+  CHECK_EQ("alejandro\nfor\nero\ncuervo",
+           ToByteString(contents.ToString()));
+  CHECK_EQ("alejandro\nforero\ncuervo",
+           ToByteString(copy->ToString()));
+}
+}  // namespace
+
+void BufferContentsTests() {
+  LOG(INFO) << "BufferContents tests: start.";
+  TestBufferContentsSnapshot();
+  LOG(INFO) << "BufferContents tests: done.";
+}
+
+}  // namespace testing
+}  // namespace editor
+}  // namespace afc
---
+++ src/test/buffer_contents_test.h
@@ -0,0 +1,12 @@
+#ifndef __AFC_EDITOR_TEST_BUFFER_CONTENTS_TEST_H__
+#define __AFC_EDITOR_TEST_BUFFER_CONTENTS_TEST_H__
+
+namespace afc {
+namespace editor {
+namespace testing {
+void BufferContentsTests();
+}  // namespace testing
+}  // namespace editor
+}  // namespace afc
+
+#endif  // __AFC_EDITOR_TEST_BUFFER_CONTENTS_TEST_H__
