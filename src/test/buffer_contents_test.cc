6904d52444094f34fe2fc32dba17dc2815b22023
Alejandro Forero Cuervo <alefore@gmail.com>
2017-04-23 22:00:27 +0200

    More unit tests.

--- src/test/buffer_contents_test.cc
+++ src/test/buffer_contents_test.cc
@@ -31,11 +31,43 @@ void TestBufferContentsSnapshot() {
   CHECK_EQ("alejandro\nforero\ncuervo",
            ToByteString(copy->ToString()));
 }
+
+void TestBufferInsertModifiers() {
+  BufferContents contents;
+  Line::Options options;
+  options.contents = NewCopyString(L"alejo");
+  options.modifiers.assign(5, {Line::CYAN});
+
+  contents.push_back(std::make_shared<Line>(options));
+  contents.push_back(std::make_shared<Line>(options));
+  options.modifiers[2].insert(Line::BOLD);
+  contents.push_back(std::make_shared<Line>(options));
+  auto line = std::make_shared<Line>(*contents.at(1));
+  line->SetAllModifiers(Line::ModifiersSet({Line::DIM}));
+  contents.push_back(line);
+
+  CHECK_EQ(contents.size(), 4);
+
+  CHECK(contents.at(0)->modifiers()[0] == Line::ModifiersSet({Line::CYAN}));
+  CHECK(contents.at(0)->modifiers()[1] == Line::ModifiersSet({Line::CYAN}));
+  CHECK(contents.at(0)->modifiers()[2] == Line::ModifiersSet({Line::CYAN}));
+
+  CHECK(contents.at(1)->modifiers()[0] == Line::ModifiersSet({Line::CYAN}));
+  CHECK(contents.at(1)->modifiers()[2] == Line::ModifiersSet({Line::CYAN}));
+
+  CHECK(contents.at(2)->modifiers()[0] == Line::ModifiersSet({Line::CYAN}));
+  CHECK(contents.at(2)->modifiers()[2] ==
+            Line::ModifiersSet({Line::CYAN, Line::BOLD}));
+
+  CHECK(contents.at(3)->modifiers()[0] == Line::ModifiersSet({Line::DIM}));
+  CHECK(contents.at(3)->modifiers()[2] == Line::ModifiersSet({Line::DIM}));
+}
 }  // namespace
 
 void BufferContentsTests() {
   LOG(INFO) << "BufferContents tests: start.";
   TestBufferContentsSnapshot();
+  TestBufferInsertModifiers();
   LOG(INFO) << "BufferContents tests: done.";
 }
 
