227bdf74ce3841759cbf0a8238a361da5087664b
Alejandro Forero Cuervo <alefore@gmail.com>
2018-11-15 22:29:55 +0100

    Fix: ensure the expression will survive until the callback.

--- src/vm/internal/append_expression.cc
+++ src/vm/internal/append_expression.cc
@@ -10,28 +10,29 @@ namespace {
 
 class AppendExpression : public Expression {
  public:
-  AppendExpression(std::unique_ptr<Expression> e0,
-                   std::unique_ptr<Expression> e1)
+  AppendExpression(std::shared_ptr<Expression> e0,
+                   std::shared_ptr<Expression> e1)
       : e0_(std::move(e0)), e1_(std::move(e1)) {}
 
   const VMType& type() { return e1_->type(); }
 
   void Evaluate(Trampoline* trampoline) override {
     // TODO: Use unique_ptr and capture by std::move.
-    std::shared_ptr<Expression> e1_copy = e1_->Clone();
-    trampoline->Bounce(e0_.get(),
-        [e1_copy](std::unique_ptr<Value>, Trampoline* trampoline) {
+    std::shared_ptr<Expression> e0_copy = e0_;
+    std::shared_ptr<Expression> e1_copy = e1_;
+    trampoline->Bounce(e0_copy.get(),
+        [e0_copy, e1_copy](std::unique_ptr<Value>, Trampoline* trampoline) {
           e1_copy->Evaluate(trampoline);
         });
   }
 
   std::unique_ptr<Expression> Clone() override {
-    return std::make_unique<AppendExpression>(e0_->Clone(), e1_->Clone());
+    return std::make_unique<AppendExpression>(e0_, e1_);
   }
 
  private:
-  const std::unique_ptr<Expression> e0_;
-  const std::unique_ptr<Expression> e1_;
+  const std::shared_ptr<Expression> e0_;
+  const std::shared_ptr<Expression> e1_;
 };
 
 }  // namespace
