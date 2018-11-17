50b2dbac6c3fd2dafc77fdab5be623dc991ad59a
Alejandro Forero Cuervo <alefore@gmail.com>
2018-11-17 23:18:09 +0100

    Improve handling of bindings.

--- rc/hooks/navigation-buffer-reload.cc
+++ rc/hooks/navigation-buffer-reload.cc
@@ -2,10 +2,18 @@ SetStatus("Enjoy your navigation.");
 
 void IncrementDepth() {
   navigation_buffer_depth = navigation_buffer_depth + 1;
+  buffer.Reload();
+  SetStatus("We must go deeper (" + tostring(navigation_buffer_depth) + ")");
 }
 
 void DecrementDepth() {
+  if (navigation_buffer_depth == 0) {
+    SetStatus("We're already at the surface.");
+    return;
+  }
   navigation_buffer_depth = navigation_buffer_depth - 1;
+  buffer.Reload();
+  SetStatus("Simplifying view (" + tostring(navigation_buffer_depth) + ")");
 }
 
 buffer.AddBinding("sj", IncrementDepth);
