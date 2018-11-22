349bbc353af271aee7017ac7848a20cf1ca42a0e
Alejandro Forero Cuervo <alefore@gmail.com>
2018-11-22 03:46:52 +0100

    Automatically reload after clang-format.

--- rc/hooks/buffer-save.cc
+++ rc/hooks/buffer-save.cc
@@ -3,7 +3,10 @@ int dot = path.find_last_of(".", path.size());
 string extension = dot == -1 ? "" : path.substr(dot + 1, path.size() - dot - 1);
 
 if (extension == "cc" || extension == "h") {
-  ForkCommand("clang-format -i " + path, false);
+  ForkCommand("clang-format -i " + path +
+                  "; sleep 1 | edge --run 'OpenFile(\"" + path +
+                  "\"); CurrentBuffer().Reload();'",
+              false);
 }
 
 if (path.starts_with("/home/alejo/edge/src")) {
