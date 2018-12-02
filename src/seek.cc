ff77e83b0047586118d026d5576e8d7bf889df21
Alejandro Forero Cuervo <alefore@gmail.com>
2018-12-02 23:05:30 +0100

    clang-format.

--- src/seek.cc
+++ src/seek.cc
@@ -18,9 +18,7 @@ Seek& Seek::WithDirection(Direction direction) {
   return *this;
 }
 
-Seek& Seek::Backwards() {
-  return WithDirection(BACKWARDS);
-}
+Seek& Seek::Backwards() { return WithDirection(BACKWARDS); }
 
 Seek::Result Seek::Once() const {
   return Advance(position_) ? DONE : UNABLE_TO_ADVANCE;
@@ -46,7 +44,9 @@ Seek::Result Seek::UntilCurrentCharNotIn(const wstring& word_char) const {
 
 Seek::Result Seek::UntilNextCharIn(const wstring& word_char) const {
   auto next_char = *position_;
-  if (!Advance(&next_char)) { return UNABLE_TO_ADVANCE; }
+  if (!Advance(&next_char)) {
+    return UNABLE_TO_ADVANCE;
+  }
   while (word_char.find(buffer_.character_at(next_char)) == word_char.npos) {
     *position_ = next_char;
     if (!Advance(&next_char)) {
@@ -58,7 +58,9 @@ Seek::Result Seek::UntilNextCharIn(const wstring& word_char) const {
 
 Seek::Result Seek::UntilNextCharNotIn(const wstring& word_char) const {
   auto next_char = *position_;
-  if (!Advance(&next_char)) { return UNABLE_TO_ADVANCE; }
+  if (!Advance(&next_char)) {
+    return UNABLE_TO_ADVANCE;
+  }
   while (word_char.find(buffer_.character_at(next_char)) != word_char.npos) {
     *position_ = next_char;
     if (!Advance(&next_char)) {
@@ -88,9 +90,7 @@ Seek::Result Seek::UntilLine(
 
 std::function<bool(const Line& line)> Negate(
     std::function<bool(const Line& line)> predicate) {
-  return [predicate](const Line& line) {
-    return !predicate(line);
-  };
+  return [predicate](const Line& line) { return !predicate(line); };
 }
 
 std::function<bool(const Line& line)> IsLineSubsetOf(
@@ -114,9 +114,7 @@ Seek::Result Seek::UntilNextLineIsNotSubsetOf(
   return UntilLine(Negate(IsLineSubsetOf(allowed_chars)));
 }
 
-wchar_t Seek::CurrentChar() const {
-  return buffer_.character_at(*position_);
-}
+wchar_t Seek::CurrentChar() const { return buffer_.character_at(*position_); }
 
 bool Seek::AdvanceLine(LineColumn* position) const {
   switch (direction_) {
@@ -147,7 +145,7 @@ bool Seek::Advance(LineColumn* position) const {
       if (buffer_.empty() || buffer_.at_end(*position)) {
         return false;
       } else if (!buffer_.at_end_of_line(*position)) {
-        position->column ++;
+        position->column++;
       } else if (!wrapping_lines_) {
         return false;
       } else {
