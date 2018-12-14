c678e0404b46eb787281915a150ffc97e618249f
Alejandro Forero Cuervo <alefore@gmail.com>
2018-12-14 15:46:45 +0100

    clang-format

--- src/parse_tree.cc
+++ src/parse_tree.cc
@@ -14,19 +14,19 @@ namespace afc {
 namespace editor {
 
 std::ostream& operator<<(std::ostream& os, const ParseTree& t) {
-    os << "[ParseTree: " << t.range << ", children: ";
-    for (auto& c : t.children) {
-      os << c;
-    }
-    os << "]";
-    return os;
+  os << "[ParseTree: " << t.range << ", children: ";
+  for (auto& c : t.children) {
+    os << c;
+  }
+  os << "]";
+  return os;
 }
 
-std::unique_ptr<ParseTree, std::function<void(ParseTree*)>>
-PushChild(ParseTree* parent) {  parent->children.emplace_back();
+std::unique_ptr<ParseTree, std::function<void(ParseTree*)>> PushChild(
+    ParseTree* parent) {
+  parent->children.emplace_back();
   return std::unique_ptr<ParseTree, std::function<void(ParseTree*)>>(
-      &parent->children.back(),
-      [parent](ParseTree* child) {
+      &parent->children.back(), [parent](ParseTree* child) {
         parent->depth = max(parent->depth, child->depth + 1);
       });
 }
@@ -56,12 +56,11 @@ void ZoomOutTree(const ParseTree& input, double ratio, ParseTree* parent) {
 }
 }  // namespace
 
-ParseTree ZoomOutTree(
-    const ParseTree& input, size_t input_lines, size_t output_lines) {
+ParseTree ZoomOutTree(const ParseTree& input, size_t input_lines,
+                      size_t output_lines) {
   LOG(INFO) << "Zooming out: " << input_lines << " to " << output_lines;
   ParseTree output;
-  ZoomOutTree(input, static_cast<double>(output_lines) / input_lines,
-              &output);
+  ZoomOutTree(input, static_cast<double>(output_lines) / input_lines, &output);
   if (output.children.empty()) {
     return ParseTree();
   }
@@ -81,8 +80,8 @@ size_t FindChildrenForPosition(const ParseTree* tree,
   return tree->children.size();
 }
 
-ParseTree::Route FindRouteToPosition(
-    const ParseTree& root, const LineColumn& position) {
+ParseTree::Route FindRouteToPosition(const ParseTree& root,
+                                     const LineColumn& position) {
   ParseTree::Route output;
   auto tree = &root;
   for (;;) {
@@ -95,9 +94,9 @@ ParseTree::Route FindRouteToPosition(
   }
 }
 
-std::vector<const ParseTree*> MapRoute(
-    const ParseTree& root, const ParseTree::Route& route) {
-  std::vector<const ParseTree*> output = { &root };
+std::vector<const ParseTree*> MapRoute(const ParseTree& root,
+                                       const ParseTree::Route& route) {
+  std::vector<const ParseTree*> output = {&root};
   for (auto& index : route) {
     output.push_back(&output.back()->children.at(index));
   }
@@ -132,12 +131,10 @@ class CharTreeParser : public TreeParser {
          line++) {
       CHECK_LT(line, buffer.size());
       auto contents = buffer.at(line);
-      size_t end_column = line == root->range.end.line
-                              ? root->range.end.column
-                              : contents->size() + 1;
-      for (size_t i = line == root->range.begin.line
-                          ? root->range.begin.column
-                          : 0;
+      size_t end_column = line == root->range.end.line ? root->range.end.column
+                                                       : contents->size() + 1;
+      for (size_t i = line == root->range.begin.line ? root->range.begin.column
+                                                     : 0;
            i < end_column; i++) {
         ParseTree new_children;
         new_children.range = Range::InLine(line, i, 1);
@@ -157,8 +154,8 @@ class WordsTreeParser : public TreeParser {
   void FindChildren(const BufferContents& buffer, ParseTree* root) override {
     CHECK(root != nullptr);
     root->children.clear();
-    for (auto line = root->range.begin.line;
-         line <= root->range.end.line; line++) {
+    for (auto line = root->range.begin.line; line <= root->range.end.line;
+         line++) {
       const auto& contents = *buffer.at(line);
 
       size_t line_end = contents.size();
@@ -171,13 +168,19 @@ class WordsTreeParser : public TreeParser {
       while (column < line_end) {
         auto new_children = PushChild(root);
 
-        while (column < line_end && IsSpace(contents, column)) { column++; }
+        while (column < line_end && IsSpace(contents, column)) {
+          column++;
+        }
         new_children->range.begin = LineColumn(line, column);
 
-        while (column < line_end && !IsSpace(contents, column)) { column++; }
+        while (column < line_end && !IsSpace(contents, column)) {
+          column++;
+        }
         new_children->range.end = LineColumn(line, column);
 
-        if (new_children->range.IsEmpty()) { return; }
+        if (new_children->range.IsEmpty()) {
+          return;
+        }
 
         DVLOG(6) << "Adding word: " << *new_children;
         delegate_->FindChildren(buffer, new_children.get());
@@ -203,8 +206,8 @@ class LineTreeParser : public TreeParser {
     CHECK(root != nullptr);
     root->children.clear();
     DVLOG(5) << "Finding lines: " << *root;
-    for (auto line = root->range.begin.line;
-         line <= root->range.end.line; line++) {
+    for (auto line = root->range.begin.line; line <= root->range.end.line;
+         line++) {
       auto contents = buffer.at(line);
       if (contents->empty()) {
         continue;
@@ -239,8 +242,8 @@ std::unique_ptr<TreeParser> NewCharTreeParser() {
 
 std::unique_ptr<TreeParser> NewWordsTreeParser(
     wstring word_characters, std::unique_ptr<TreeParser> delegate) {
-  return std::make_unique<WordsTreeParser>(
-      word_characters, std::move(delegate));
+  return std::make_unique<WordsTreeParser>(word_characters,
+                                           std::move(delegate));
 }
 
 std::unique_ptr<TreeParser> NewLineTreeParser(
