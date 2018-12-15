eee714b4edc6132ee15aed2eb048aa75bfb390db
Alejandro Forero Cuervo <alefore@gmail.com>
2018-12-15 15:11:06 +0100

    Add logging statements.

--- src/parse_tools.cc
+++ src/parse_tools.cc
@@ -9,14 +9,17 @@ void Action::Execute(std::vector<ParseTree*>* trees, size_t line) {
       trees->push_back(PushChild(trees->back()).release());
       trees->back()->range.begin = LineColumn(line, column);
       trees->back()->modifiers = modifiers;
+      DVLOG(5) << "Tree: Push: " << trees->back()->range;
       break;
 
     case POP:
       trees->back()->range.end = LineColumn(line, column);
+      DVLOG(5) << "Tree: Pop: " << trees->back()->range;
       trees->pop_back();
       break;
 
     case SET_FIRST_CHILD_MODIFIERS:
+      DVLOG(5) << "Tree: SetModifiers: " << trees->back()->range;
       trees->back()->children.front().modifiers = modifiers;
       break;
   }
