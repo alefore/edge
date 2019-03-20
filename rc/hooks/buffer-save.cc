f3fc007f56e1e527f87e0b74d61286c2ef2f960f
Alejandro Forero Cuervo <alefore@gmail.com>
2019-03-21 00:03:33 +0100

    Revamp automatic execution of clang-format.
    Add a key binding (sC) to toggle it.

--- rc/hooks/buffer-save.cc
+++ rc/hooks/buffer-save.cc
@@ -2,12 +2,7 @@ string path = buffer.path();
 int dot = path.find_last_of(".", path.size());
 string extension = dot == -1 ? "" : path.substr(dot + 1, path.size() - dot - 1);
 
-if (extension == "cc" || extension == "h" || extension == "java") {
-  ForkCommand("clang-format -i " + path.shell_escape() +
-                  "; sleep 1 | edge --run 'OpenFile(\"" + path.shell_escape() +
-                  "\"); CurrentBuffer().Reload();'",
-              false);
-}
+ClangFormatOnSave();
 
 if (path.starts_with("/home/alejo/edge/src")) {
   ForkCommand("make -j3", false);
