405820eff3dc4314cccf49cbafaf2372f191b5a5
Alejandro Forero Cuervo <alefore@gmail.com>
2019-03-23 14:19:12 +0100

    vm: Add ShapesLineColumnSet.

--- src/shapes.cc
+++ src/shapes.cc
@@ -1,18 +1,44 @@
 #include "src/shapes.h"
 
+#include <set>
+
 #include <glog/logging.h>
 
+#include "src/line_column.h"
+#include "src/vm/public/callbacks.h"
 #include "src/vm/public/types.h"
 #include "src/vm/public/value.h"
 
 namespace afc {
 namespace editor {
+struct LineColumnSet;
+}  // namespace editor
+namespace vm {
+template <>
+struct VMTypeMapper<editor::LineColumnSet*> {
+  static editor::LineColumnSet* get(Value* value) {
+    return static_cast<editor::LineColumnSet*>(value->user_value.get());
+  }
+
+  static const VMType vmtype;
+};
+
+const VMType VMTypeMapper<editor::LineColumnSet*>::vmtype =
+    VMType::ObjectType(L"ShapesLineColumnSet");
+
+}  // namespace vm
+namespace editor {
 
 using namespace afc::vm;
 
+// Coordinates are in range [0.0, 1.0).
 struct Point {
   double x;
   double y;
+};
+
+struct LineColumnSet {
+  std::set<LineColumn> positions;
 };
 
 void DefinePoint(Environment* environment) {
@@ -57,7 +83,48 @@ void DefinePoint(Environment* environment) {
   environment->DefineType(L"ShapesPoint", std::move(point_type));
 }
 
-void InitShapes(vm::Environment* environment) { DefinePoint(environment); }
+void DefineLineColumnSet(Environment* environment) {
+  auto line_column_set_type =
+      std::make_unique<ObjectType>(L"ShapesLineColumnSet");
+
+  environment->Define(
+      L"ShapesLineColumnSet",
+      Value::NewFunction({VMType::ObjectType(line_column_set_type.get())},
+                         [](std::vector<Value::Ptr> args) {
+                           CHECK(args.empty());
+                           return Value::NewObject(
+                               L"ShapesLineColumnSet",
+                               std::make_shared<LineColumnSet>());
+                         }));
+
+  line_column_set_type->AddField(
+      L"size", vm::NewCallback(std::function<int(LineColumnSet*)>(
+                   [](LineColumnSet* s) { return s->positions.size(); })));
+  line_column_set_type->AddField(
+      L"contains",
+      vm::NewCallback(std::function<bool(LineColumnSet*, LineColumn)>(
+          [](LineColumnSet* s, LineColumn p) {
+            return s->positions.count(p) > 0;
+          })));
+  line_column_set_type->AddField(
+      L"get", vm::NewCallback(std::function<LineColumn(LineColumnSet*, int)>(
+                  [](LineColumnSet* s, int i) {
+                    auto it = s->positions.begin();
+                    std::advance(it, i);
+                    return *it;
+                  })));
+  line_column_set_type->AddField(
+      L"insert",
+      vm::NewCallback(std::function<void(LineColumnSet*, LineColumn)>(
+          [](LineColumnSet* s, LineColumn e) { s->positions.insert(e); })));
+  environment->DefineType(L"ShapesLineColumnSet",
+                          std::move(line_column_set_type));
+}
+
+void InitShapes(vm::Environment* environment) {
+  DefinePoint(environment);
+  DefineLineColumnSet(environment);
+}
 
 }  // namespace editor
 }  // namespace afc
