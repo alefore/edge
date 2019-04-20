79f8a1ff70321268d73afb11d8cde594bb5467ee
Alejandro Forero Cuervo <alefore@gmail.com>
2019-04-20 14:46:04 +0200

    Move screen-output-receiver to its own module.

--- src/terminal.cc
+++ src/terminal.cc
@@ -13,6 +13,7 @@
 #include "src/output_receiver.h"
 #include "src/output_receiver_optimizer.h"
 #include "src/parse_tree.h"
+#include "src/screen_output_receiver.h"
 
 namespace afc {
 namespace editor {
@@ -354,70 +355,6 @@ wstring Terminal::GetBufferContext(const EditorState& editor_state,
   }
   return buffer->Read(buffer_variables::name());
 }
-
-class LineOutputReceiver : public OutputReceiver {
- public:
-  LineOutputReceiver(Screen* screen) : screen_(screen) {}
-
-  void AddCharacter(wchar_t c) override {
-    if (c == L'\t') {
-      // Nothing.
-    } else if (iswprint(c) || c == L'\t' || c == L'\r' || c == L'\n') {
-      screen_->WriteString(wstring(1, c));
-    } else if (wcwidth(c) <= 0) {
-      // Nothing.
-    } else {
-      screen_->WriteString(wstring(wcwidth(c), L' '));
-    }
-    RegisterChar(c);
-  }
-  void AddString(const wstring& str) override {
-    for (auto& c : str) {
-      AddCharacter(c);
-    }
-  }
-  void AddModifier(LineModifier modifier) override {
-    screen_->SetModifier(modifier);
-  }
-
-  void SetTabsStart(size_t columns) override { tabs_start_ = columns % 8; }
-
-  size_t column() override { return column_write_; }
-
-  size_t width() override { return screen_->columns(); }
-
- private:
-  void RegisterChar(wchar_t c) {
-    CHECK_LT(column_write_, screen_->columns());
-    switch (c) {
-      case L'\n':
-        column_write_ = 0;
-        break;
-      case L'\t': {
-        CHECK_GE(column_write_, tabs_start_);
-        size_t new_value =
-            tabs_start_ +
-            8 * static_cast<size_t>(
-                    1 + floor(static_cast<double>(column_write_ - tabs_start_) /
-                              8.0));
-        new_value = std::min(new_value, screen_->columns());
-        CHECK_GT(new_value, column_write_);
-        CHECK_LE(new_value - column_write_, 8u);
-        screen_->WriteString(wstring(new_value - column_write_, ' '));
-        column_write_ = new_value;
-      } break;
-      case L'​':
-        break;
-      default:
-        column_write_ += wcwidth(c);
-    }
-    column_write_ = std::min(column_write_, screen_->columns());
-  }
-
-  Screen* const screen_;
-  size_t column_write_ = 0;
-  size_t tabs_start_ = 0;
-};
 
 class WithPrefixOutputReceiver : public DelegatingOutputReceiver {
  public:
@@ -814,9 +751,9 @@ void Terminal::ShowBuffer(const EditorState* editor_state, Screen* screen) {
       buffer->Read(buffer_variables::view_start_column()));
 
   for (size_t i = 0; i < lines_to_show; i++) {
-    LineOutputReceiver screen_adapter(screen);
+    auto screen_receiver = NewScreenOutputReceiver(screen);
     auto line_output_receiver =
-        std::make_unique<OutputReceiverOptimizer>(&screen_adapter);
+        std::make_unique<OutputReceiverOptimizer>(screen_receiver.get());
 
     if (position.line >= buffer->lines_size()) {
       line_output_receiver->AddString(L"\n");
