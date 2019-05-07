cf1f4fa07ea2b8895bca03121d75702b47fbf920
Alejandro Forero Cuervo <alefore@gmail.com>
2019-05-07 13:01:07 +0200

    Importing missing viewers.

---
+++ src/viewers.cc
@@ -0,0 +1,45 @@
+#include "src/viewers.h"
+
+#include "src/wstring.h"
+
+namespace afc {
+namespace editor {
+
+Viewers::Registration Viewers::Register(LineColumnDelta view_size) {
+  // TODO: Optimize to not require 2 tree traversals?
+  bool new_item = view_sizes_.find(view_size) == view_sizes_.end();
+  view_sizes_.insert(view_size);
+  if (new_item) {
+    last_view_size_ = view_size;
+  }
+  for (auto& l : listeners_) {
+    l();
+  }
+  return std::unique_ptr<bool, std::function<void(bool*)>>(
+      new bool(), [this, view_size](bool* value) {
+        delete value;
+        auto it = view_sizes_.find(view_size);
+        CHECK(it != view_sizes_.end());
+        view_sizes_.erase(it);
+      });
+}
+
+Viewers::Registration Viewers::AddListener(std::function<void()> listener) {
+  listeners_.push_front(listener);
+  return std::unique_ptr<bool, std::function<void(bool*)>>(
+      new bool(), [this, it = listeners_.begin()](bool* value) {
+        delete value;
+        listeners_.erase(it);
+      });
+}
+
+std::set<LineColumnDelta> Viewers::GetViewSizes() const {
+  return std::set<LineColumnDelta>(view_sizes_.begin(), view_sizes_.end());
+}
+
+std::optional<LineColumnDelta> Viewers::last_view_size() const {
+  return last_view_size_;
+}
+
+}  // namespace editor
+}  // namespace afc
---
+++ src/viewers.h
@@ -0,0 +1,50 @@
+#ifndef __AFC_EDITOR_VIEWERS_H__
+#define __AFC_EDITOR_VIEWERS_H__
+
+#include <functional>
+#include <list>
+#include <memory>
+#include <optional>
+#include <set>
+
+#include "src/line_column.h"
+
+namespace afc {
+namespace editor {
+
+// Not thread safe.
+class Viewers {
+ public:
+  using Registration = std::unique_ptr<bool, std::function<void(bool*)>>;
+
+  // When a widget with a given display size wants to register interest in a
+  // buffer, it should call `Register`. It should hold the returned object
+  // until it's done (either a new size is requested, or it switches to a new
+  // buffer).
+  Registration Register(LineColumnDelta view_size);
+
+  // Adds a callback that will be updated whenever last_view_size_ changes.
+  // The callback will be executed until the time when the customer deletes the
+  // returned value. Once the first such callback runs, last_view_size_ will
+  // always have a value.
+  Registration AddListener(std::function<void()> listener);
+
+  std::set<LineColumnDelta> GetViewSizes() const;
+
+  std::optional<LineColumnDelta> last_view_size() const;
+
+ private:
+  // Holds the last value added to view_sizes_ when the same value didn't
+  // already exist there. This is a good heuristic to try to select the last
+  // active screen.
+  //
+  // TODO: Instead of this, use the last one that has had input.
+  std::optional<LineColumnDelta> last_view_size_;
+  std::multiset<LineColumnDelta> view_sizes_;
+  std::list<std::function<void()>> listeners_;
+};
+
+}  // namespace editor
+}  // namespace afc
+
+#endif  // __AFC_EDITOR_VIEWERS_H__
