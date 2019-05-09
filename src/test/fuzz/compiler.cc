f4021ca263c43898016048f00cb9b5354410f610
Alejandro Forero Cuervo <alefore@gmail.com>
2019-05-09 09:18:29 +0200

    Import fuzz tests for our C++ syntax highlighter and for the VM.

---
+++ src/test/fuzz/compiler.cc
@@ -0,0 +1,28 @@
+#include <glog/logging.h>
+
+#include <csignal>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+extern "C" {
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+}
+
+#include "src/buffer_contents.h"
+#include "src/char_buffer.h"
+#include "src/cpp_parse_tree.h"
+#include "src/lazy_string.h"
+#include "src/vm/public/vm.h"
+
+using namespace afc::editor;
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  std::wifstream input(argv[1]);
+  wstring error;
+  afc::vm::CompileFile(argv[1], afc::vm::Environment::GetDefault(), &error);
+  return 0;
+}
---
+++ src/test/fuzz/parser.cc
@@ -0,0 +1,43 @@
+#include <glog/logging.h>
+
+#include <csignal>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+extern "C" {
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+}
+
+#include "src/buffer_contents.h"
+#include "src/char_buffer.h"
+#include "src/cpp_parse_tree.h"
+#include "src/lazy_string.h"
+
+using namespace afc::editor;
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  auto parser = NewCppTreeParser(
+      {L"auto", L"int", L"char", L"if", L"while", L"const", L"for"},
+      {L"optoins"});
+
+  std::wifstream input(argv[1]);
+
+  BufferContents contents;
+  for (std::wstring line; std::getline(input, line, L'\n');) {
+    Line::Options options;
+    options.contents = NewLazyString(line);
+    contents.AppendToLine(contents.EndLine(), Line(std::move(options)));
+    contents.push_back(std::make_shared<Line>());
+  }
+
+  std::cout << "Parsing input: " << contents.ToString();
+
+  ParseTree tree;
+  parser->FindChildren(contents, &tree);
+
+  return 0;
+}
