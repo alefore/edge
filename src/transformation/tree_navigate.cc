7358cb8d311743852243daabb40eff4d3e57cbfe
Alejandro Forero Cuervo <alefore@gmail.com>
2019-12-26 19:42:17 +0100

    Make TreeNavigate use CompositeTransformation.
    While at it, change '%' to go to the end of the tree, rather than the beginning.

--- src/transformation/tree_navigate.cc
+++ src/transformation/tree_navigate.cc
@@ -4,33 +4,30 @@
 #include "src/parse_tree.h"
 #include "src/seek.h"
 #include "src/transformation/composite.h"
+#include "src/transformation/set_position.h"
 #include "src/vm_transformation.h"
 
 namespace afc::editor {
 namespace {
-class TreeNavigate : public Transformation {
-  void Apply(Result* result) const override {
-    CHECK(result != nullptr);
-    CHECK(result->buffer != nullptr);
-    auto root = result->buffer->parse_tree();
-    if (root == nullptr) {
-      result->success = false;
-      return;
-    }
+class TreeNavigate : public CompositeTransformation {
+  std::wstring Serialize() const override { return L"TreeNavigate()"; }
+  void Apply(Input input) const override {
+    auto root = input.buffer->parse_tree();
+    if (root == nullptr) return;
     const ParseTree* tree = root.get();
-    auto next_position = result->cursor;
-    Seek(*result->buffer->contents(), &next_position).Once();
+    auto next_position = input.position;
+    Seek(*input.buffer->contents(), &next_position).Once();
     while (true) {
       size_t child = 0;
       while (child < tree->children().size() &&
-             (tree->children()[child].range().end <= result->cursor ||
+             (tree->children()[child].range().end <= input.position ||
               tree->children()[child].children().empty())) {
         child++;
       }
       if (child < tree->children().size()) {
         bool descend = false;
         auto candidate = &tree->children()[child];
-        if (tree->range().begin < result->cursor) {
+        if (tree->range().begin < input.position) {
           descend = true;
         } else if (tree->range().end == next_position) {
           descend = candidate->range().end == next_position;
@@ -43,24 +40,20 @@ class TreeNavigate : public Transformation {
       }
 
       auto last_position = tree->range().end;
-      Seek(*result->buffer->contents(), &last_position).Backwards().Once();
-
-      auto original_cursor = result->cursor;
-      result->cursor = result->cursor < tree->range().begin ||
-                               result->cursor == last_position
-                           ? tree->range().begin
-                           : last_position;
-      result->success = original_cursor != result->cursor;
+      Seek(*input.buffer->contents(), &last_position).Backwards().Once();
+      input.push(NewSetPositionTransformation(input.position == last_position
+                                                  ? tree->range().begin
+                                                  : last_position));
       return;
     }
   }
 
-  std::unique_ptr<Transformation> Clone() const override {
+  std::unique_ptr<CompositeTransformation> Clone() const override {
     return std::make_unique<TreeNavigate>();
   }
 };
 }  // namespace
 std::unique_ptr<Transformation> NewTreeNavigateTransformation() {
-  return std::make_unique<TreeNavigate>();
+  return NewTransformation(Modifiers(), std::make_unique<TreeNavigate>());
 }
 }  // namespace afc::editor
