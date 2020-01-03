b5cfbf9d30d0304ac8643fe4243fa3704b877566
Alejandro Forero Cuervo <alefore@gmail.com>
2020-01-03 20:28:37 +0100

    Fix inline lambda forms: correctly refer to their lexical scope.
    To do this, I had to use shared_ptr for Environment instances.

--- src/vm/internal/lambda.cc
+++ src/vm/internal/lambda.cc
@@ -8,62 +8,7 @@
 #include "../public/value.h"
 
 namespace afc::vm {
-namespace {
-std::unique_ptr<Value> NewFunctionValue(
-    VMType expected_return_type, std::vector<VMType> argument_types,
-    std::vector<std::wstring> argument_names, std::unique_ptr<Expression> body,
-    std::shared_ptr<Environment> environment, std::wstring* error) {
-  auto deduced_types = body->ReturnTypes();
-  if (deduced_types.empty()) {
-    deduced_types.insert(VMType::Void());
-  }
-  if (deduced_types.size() > 1) {
-    *error = L"Found multiple return types: ";
-    std::wstring separator;
-    for (const auto& type : deduced_types) {
-      *error += separator + L"`" + type.ToString() + L"`";
-      separator = L", ";
-    }
-    return nullptr;
-  } else if (deduced_types.find(expected_return_type) == deduced_types.end()) {
-    *error = L"Expected a return type of `" + expected_return_type.ToString() +
-             L"` but found `" + deduced_types.cbegin()->ToString() + L"`.";
-    return nullptr;
-  }
-
-  auto output = std::make_unique<Value>(VMType::FUNCTION);
-  output->type.type_arguments.push_back(expected_return_type);
-  for (auto& argument_type : argument_types) {
-    output->type.type_arguments.push_back(argument_type);
-  }
-  output->callback =
-      [body = std::shared_ptr<Expression>(std::move(body)), environment,
-       argument_names](vector<unique_ptr<Value>> args, Trampoline* trampoline) {
-        CHECK_EQ(args.size(), argument_names.size())
-            << "Invalid number of arguments for function.";
-        for (size_t i = 0; i < args.size(); i++) {
-          environment->Define(argument_names[i], std::move(args[i]));
-        }
-        trampoline->SetReturnContinuation(
-            [original_trampoline = *trampoline](std::unique_ptr<Value> value,
-                                                Trampoline* trampoline) {
-              CHECK(value != nullptr);
-              // We have to make a copy because assigning to *trampoline may
-              // delete us (and thus deletes original_trampoline as it is being
-              // read).
-              Trampoline tmp_copy = original_trampoline;
-              *trampoline = tmp_copy;
-              trampoline->Return(std::move(value));
-            });
-        trampoline->SetEnvironment(environment.get());
-        trampoline->Bounce(body.get(), body->Types()[0],
-                           [body](Value::Ptr value, Trampoline* trampoline) {
-                             trampoline->Return(std::move(value));
-                           });
-      };
-  return output;
-}
-}  // namespace
+namespace {}  // namespace
 
 std::unique_ptr<UserFunction> UserFunction::New(
     Compilation* compilation, std::wstring return_type,
@@ -103,16 +48,63 @@ std::unique_ptr<UserFunction> UserFunction::New(
 std::unique_ptr<Value> UserFunction::BuildValue(
     Compilation* compilation, std::unique_ptr<Expression> body,
     std::wstring* error) {
-  auto function_environment =
-      std::make_shared<Environment>(compilation->environment);
+  auto environment = std::make_shared<Environment>(compilation->environment);
   compilation->environment = compilation->environment->parent_environment();
 
   std::vector<VMType> argument_types(type.type_arguments.cbegin() + 1,
                                      type.type_arguments.cend());
 
-  return NewFunctionValue(*type.type_arguments.cbegin(), argument_types,
-                          argument_names, std::move(body),
-                          std::move(function_environment), error);
+  VMType expected_return_type = *type.type_arguments.cbegin();
+  auto deduced_types = body->ReturnTypes();
+  if (deduced_types.empty()) {
+    deduced_types.insert(VMType::Void());
+  }
+  if (deduced_types.size() > 1) {
+    *error = L"Found multiple return types: ";
+    std::wstring separator;
+    for (const auto& type : deduced_types) {
+      *error += separator + L"`" + type.ToString() + L"`";
+      separator = L", ";
+    }
+    return nullptr;
+  } else if (deduced_types.find(expected_return_type) == deduced_types.end()) {
+    *error = L"Expected a return type of `" + expected_return_type.ToString() +
+             L"` but found `" + deduced_types.cbegin()->ToString() + L"`.";
+    return nullptr;
+  }
+
+  auto output = std::make_unique<Value>(VMType::FUNCTION);
+  output->type.type_arguments.push_back(expected_return_type);
+  for (auto& argument_type : argument_types) {
+    output->type.type_arguments.push_back(argument_type);
+  }
+  output->callback = [body = std::shared_ptr<Expression>(std::move(body)),
+                      environment, argument_names = argument_names](
+                         vector<unique_ptr<Value>> args,
+                         Trampoline* trampoline) {
+    CHECK_EQ(args.size(), argument_names.size())
+        << "Invalid number of arguments for function.";
+    for (size_t i = 0; i < args.size(); i++) {
+      environment->Define(argument_names[i], std::move(args[i]));
+    }
+    trampoline->SetReturnContinuation(
+        [original_trampoline = *trampoline](std::unique_ptr<Value> value,
+                                            Trampoline* trampoline) {
+          CHECK(value != nullptr);
+          // We have to make a copy because assigning to *trampoline may
+          // delete us (and thus deletes original_trampoline as it is being
+          // read).
+          Trampoline tmp_copy = original_trampoline;
+          *trampoline = tmp_copy;
+          trampoline->Return(std::move(value));
+        });
+    trampoline->SetEnvironment(environment.get());
+    trampoline->Bounce(body.get(), body->Types()[0],
+                       [body](Value::Ptr value, Trampoline* trampoline) {
+                         trampoline->Return(std::move(value));
+                       });
+  };
+  return output;
 }
 
 std::unique_ptr<Expression> UserFunction::BuildExpression(
