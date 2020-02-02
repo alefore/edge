783ea126e4aae9f46b4543c4c1b5a9eb784f7722
Alejandro Forero Cuervo <alefore@gmail.com>
2020-02-02 23:38:45 +0100

    Improve handling of too many open/displayed files.

--- src/widget_list.cc
+++ src/widget_list.cc
@@ -153,6 +153,8 @@ wstring WidgetListHorizontal::ToString() const {
 
 std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
     OutputProducerOptions options) const {
+  if (options.size.line.IsZero()) return OutputProducer::Empty();
+
   std::vector<LineNumberDelta> lines_per_child;
   for (auto& child : children_) {
     lines_per_child.push_back(child->MinimumLines());
@@ -171,29 +173,35 @@ std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
   LineNumberDelta lines_given = std::accumulate(
       lines_per_child.begin(), lines_per_child.end(), LineNumberDelta(0));
 
+  // The total number of lines to give to all children. Will exclude additional
+  // kInformationLines when children are skipped.
+  auto lines_available = options.size.line;
+
   // TODO: this could be done way faster (sort + single pass over all
   // buffers).
-  while (lines_given > options.size.line) {
-    LOG(INFO) << "Ensuring that lines given (" << lines_given
-              << ") doesn't exceed lines available (" << options.size.line
-              << ").";
-    std::vector<size_t> indices_maximal_producers = {0};
-    for (size_t i = 1; i < lines_per_child.size(); i++) {
-      LineNumberDelta maximum =
-          lines_per_child[indices_maximal_producers.front()];
-      if (maximum < lines_per_child[i]) {
-        indices_maximal_producers = {i};
-      } else if (maximum == lines_per_child[i]) {
-        indices_maximal_producers.push_back(i);
+  if (lines_given > options.size.line) {
+    static const LineNumberDelta kInformationLines = LineNumberDelta(1);
+    lines_available -= kInformationLines;
+    while (lines_given > lines_available) {
+      size_t index_maximal_lines = 0;
+      for (size_t i = 1; i < lines_per_child.size(); i++) {
+        LineNumberDelta maximum = lines_per_child[index_maximal_lines];
+        if (maximum < lines_per_child[i] ||
+            (index_maximal_lines == active_ && !lines_per_child[i].IsZero())) {
+          index_maximal_lines = i;
+        }
       }
-    }
-    CHECK(!indices_maximal_producers.empty());
-    CHECK_GT(lines_per_child[indices_maximal_producers[0]], LineNumberDelta(0));
-    for (auto& i : indices_maximal_producers) {
-      if (lines_given > options.size.line) {
-        lines_given--;
-        lines_per_child[i]--;
+
+      CHECK_LT(index_maximal_lines, lines_per_child.size());
+      if (lines_given == lines_per_child[index_maximal_lines]) {
+        // This child is the only child receiving any lines.
+        lines_given = lines_available;
+        lines_per_child[index_maximal_lines] = lines_available;
+        continue;
       }
+
+      lines_given -= lines_per_child[index_maximal_lines];
+      lines_per_child[index_maximal_lines] = LineNumberDelta();
     }
   }
 
@@ -201,12 +209,18 @@ std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
            std::accumulate(lines_per_child.begin(), lines_per_child.end(),
                            LineNumberDelta(0)));
 
-  if (options.size.line > lines_given) {
-    LineNumberDelta lines_each =
-        (options.size.line - lines_given) / lines_per_child.size();
-    lines_given += lines_per_child.size() * lines_each;
+  if (lines_given.IsZero()) return OutputProducer::Empty();
+
+  size_t children_skipped = std::count(
+      lines_per_child.begin(), lines_per_child.end(), LineNumberDelta());
+
+  if (lines_available > lines_given) {
+    LineNumberDelta lines_each = (lines_available - lines_given) /
+                                 (lines_per_child.size() - children_skipped);
+    lines_given += (lines_per_child.size() - children_skipped) * lines_each;
     for (auto& l : lines_per_child) {
-      LineNumberDelta extra_line = lines_given < options.size.line
+      if (l.IsZero()) continue;
+      LineNumberDelta extra_line = lines_given < lines_available
                                        ? LineNumberDelta(1)
                                        : LineNumberDelta(0);
       l += lines_each + extra_line;
@@ -214,17 +228,20 @@ std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
     }
   }
 
-  CHECK_EQ(options.size.line,
+  CHECK_EQ(lines_available,
            std::accumulate(lines_per_child.begin(), lines_per_child.end(),
                            LineNumberDelta(0)));
 
   std::vector<HorizontalSplitOutputProducer::Row> rows;
   CHECK_EQ(children_.size(), lines_per_child.size());
   for (size_t index = 0; index < children_.size(); index++) {
-    std::shared_ptr<const OpenBuffer> buffer =
-        children_[index]->GetActiveLeaf()->Lock();
     OutputProducerOptions child_options = options;
     child_options.size.line = lines_per_child[index];
+    if (child_options.size.line.IsZero()) {
+      continue;
+    }
+    std::shared_ptr<const OpenBuffer> buffer =
+        children_[index]->GetActiveLeaf()->Lock();
     std::unique_ptr<OutputProducer> child_producer;
     if (children_.size() > 1) {
       VLOG(5) << "Producing row with frame.";
@@ -239,6 +256,8 @@ std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
       if (buffer != nullptr) {
         frame_options.extra_information =
             OpenBuffer::FlagsToString(buffer->Flags());
+        frame_options.width =
+            ColumnNumberDelta(buffer->Read(buffer_variables::line_width));
       }
       nested_rows.push_back(
           {std::make_unique<FrameOutputProducer>(std::move(frame_options)),
@@ -260,8 +279,22 @@ std::unique_ptr<OutputProducer> WidgetListHorizontal::CreateOutputProducer(
     rows.push_back({std::move(child_producer), lines_per_child[index]});
   }
 
-  return std::make_unique<HorizontalSplitOutputProducer>(std::move(rows),
-                                                         active_);
+  if (children_skipped > 0) {
+    rows.push_back(
+        {std::make_unique<FrameOutputProducer>(
+             FrameOutputProducer::FrameOptions{
+                 .title =
+                     L"Additional files: " + std::to_wstring(children_skipped),
+                 .active_state =
+                     FrameOutputProducer::FrameOptions::ActiveState::kActive}),
+         LineNumberDelta(1)});
+  }
+
+  size_t children_skipped_before_active =
+      std::count(lines_per_child.cbegin(), lines_per_child.cbegin() + active_,
+                 LineNumberDelta());
+  return std::make_unique<HorizontalSplitOutputProducer>(
+      std::move(rows), active_ - children_skipped_before_active);
 }
 
 LineNumberDelta WidgetListHorizontal::MinimumLines() const {
