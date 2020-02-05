b9dd97382b01de7efa6ee9dcb21cc4fb54c1a25a
Alejandro Forero Cuervo <alefore@gmail.com>
2020-02-06 00:10:22 +0100

    Set `persist_state` to false for the console buffer.

--- src/editor.cc
+++ src/editor.cc
@@ -857,10 +857,12 @@ static BufferPosition PositionFromLine(const wstring& line) {
 std::shared_ptr<OpenBuffer> EditorState::GetConsole() {
   auto it = buffers_.insert(make_pair(L"- console", nullptr));
   if (it.second) {  // Inserted the entry.
+    CHECK(it.first->second == nullptr);
     it.first->second =
         OpenBuffer::New({.editor = this, .name = it.first->first});
     it.first->second->Set(buffer_variables::allow_dirty_delete, true);
     it.first->second->Set(buffer_variables::show_in_buffers_list, false);
+    it.first->second->Set(buffer_variables::persist_state, false);
   }
   return it.first->second;
 }
