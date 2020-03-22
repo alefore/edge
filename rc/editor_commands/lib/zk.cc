d3ec310f100940ef4d89b58e767a397017c0e172
Alejandro Forero Cuervo <alefore@gmail.com>
2020-03-22 13:34:31 +0100

    Zk: Add a new command: :zkl
    Turns a path like:
      098.md
    Into a link to it, extracting the text of the link from the first line there:
      [Iteration: Why](098.md)

--- rc/editor_commands/lib/zk.cc
+++ rc/editor_commands/lib/zk.cc
@@ -1,6 +1,7 @@
 // This file contains functions that I use to manage my Zettelkasten.
 
 #include "paths.cc"
+#include "strings.cc"
 
 void zkRunCommand(string name, string command) {
   ForkCommandOptions options = ForkCommandOptions();
@@ -36,4 +37,50 @@ void zkn() {
       "edge -X $( find . -size 0b -name '???.md' | sort | head -1 )");
   options.set_insertion_type("ignore");
   ForkCommand(options);
+}
+
+string GetNoteTitle(string path) {
+  auto buffer = OpenFile(path, false);
+  buffer.WaitForEndOfFile();
+  auto line = buffer.line(0);
+  if (line.substr(0, 1) == "#") {
+    line = line.substr(1, line.size() - 1);
+  }
+  return SkipSpaces(line);
+}
+
+TransformationOutput ZKInternalLink(Buffer buffer, TransformationInput input) {
+  auto line = buffer.line(input.position().line());
+  auto path_characters = buffer.path_characters();
+  int start = line.find_last_not_of(path_characters, input.position().column());
+  if (start == -1) {
+    start = 0;  // Didn't find, must be at beginning.
+  } else {
+    start++;
+  }
+  int end = line.find_first_not_of(path_characters, start);
+  if (end == -1) {
+    end = line.size();
+  }
+
+  string path = line.substr(start, end - start);
+
+  return TransformationOutput()
+      .push(SetColumnTransformation(end))
+      .push(InsertTransformationBuilder().set_text(")").build())
+      .push(SetColumnTransformation(start))
+      .push(InsertTransformationBuilder().set_text("[](").build())
+      .push(SetColumnTransformation(start + 1))
+      .push(InsertTransformationBuilder().set_text(GetNoteTitle(path)).build());
+}
+
+// Replaces a path (e.g., `03d.md` with a link to it, extracting the text of the
+// link from the first line in the file.
+void zkl() {
+  editor.ForEachActiveBuffer([](Buffer buffer) -> void {
+    buffer.ApplyTransformation(FunctionTransformation(
+        [](TransformationInput input) -> TransformationOutput {
+          return ZKInternalLink(buffer, input);
+        }));
+  });
 }
