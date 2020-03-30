09b3cd7c182515b88e5212a678640bd230257c3e
Alejandro Forero Cuervo <alefore@gmail.com>
2020-03-30 19:39:49 +0200

    Add `Extension` function.

--- rc/editor_commands/lib/paths.cc
+++ rc/editor_commands/lib/paths.cc
@@ -13,3 +13,11 @@ string Dirname(string path) {
   }
   return path.substr(0, last_slash);
 }
+
+string Extension(string path) {
+  int last_dot = path.find_last_of(".", path.size());
+  if (last_dot == -1) {
+    return "";
+  }
+  return path.substr(last_dot + 1, path.size() - (last_dot + 1));
+}
