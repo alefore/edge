4ef7b67c41996708f0bf78b1ed8ef3df86edb314
Alejandro Forero Cuervo <alefore@gmail.com>
2020-04-04 21:07:05 +0200

    Add tests for ConstTree: RandomWalk.
    Inserts elements at random positions until the tree grows to 10k elements, and
    then removes them.

--- src/const_tree.cc
+++ src/const_tree.cc
@@ -1,14 +1,16 @@
 #include "src/const_tree.h"
 
 #include "src/tests/benchmarks.h"
+#include "src/tests/tests.h"
 #include "src/time.h"
 
 namespace afc::editor {
-
-ConstTree<int>::Ptr GetTree(int size) {
-  ConstTree<int>::Ptr tree;
+namespace {
+using IntTree = ConstTree<int>;
+IntTree::Ptr GetTree(int size) {
+  IntTree::Ptr tree;
   for (int i = 0; i < size; ++i) {
-    tree = ConstTree<int>::PushBack(tree, 0);
+    tree = IntTree::PushBack(tree, 0);
   }
   return tree;
 }
@@ -17,9 +19,9 @@ bool registration_add =
     tests::RegisterBenchmark(L"ConstTree::PushBack", [](int elements) {
       auto tree = GetTree(elements);
       auto start = Now();
-      tree = ConstTree<int>::PushBack(tree, 0);
+      tree = IntTree::PushBack(tree, 0);
       auto end = Now();
-      CHECK_EQ(ConstTree<int>::Size(tree), static_cast<size_t>(elements) + 1);
+      CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements) + 1);
       return SecondsBetween(start, end);
     });
 
@@ -27,9 +29,44 @@ bool registration_prefix =
     tests::RegisterBenchmark(L"ConstTree::Prefix", [](int elements) {
       auto tree = GetTree(elements);
       auto start = Now();
-      tree = ConstTree<int>::Prefix(tree, elements - 1);
+      tree = IntTree::Prefix(tree, elements - 1);
       auto end = Now();
-      CHECK_EQ(ConstTree<int>::Size(tree), static_cast<size_t>(elements) - 1);
+      CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements) - 1);
       return SecondsBetween(start, end);
     });
+
+IntTree::Ptr Insert(IntTree::Ptr tree, size_t position) {
+  static const int kNumberToInsert = 25;
+  return IntTree::Append(
+      IntTree::PushBack(IntTree::Prefix(tree, position), kNumberToInsert),
+      IntTree::Suffix(tree, position));
+}
+
+IntTree::Ptr Remove(IntTree::Ptr tree, size_t position) {
+  return IntTree::Append(IntTree::Prefix(tree, position),
+                         IntTree::Suffix(tree, position + 1));
+}
+
+class ConstTreeTests : public tests::TestGroup<ConstTreeTests> {
+ public:
+  ConstTreeTests() : TestGroup<ConstTreeTests>() {}
+  std::wstring Name() const override { return L"ConstTreeTests"; }
+  std::vector<tests::Test> Tests() const override {
+    // Tests that the invariants (about balance of the tree) hold.
+    return {{.name = L"RandomWalk", .callback = [] {
+               IntTree::Ptr tree;
+               while (IntTree::Size(tree) < 1e4) {
+                 tree = Insert(tree, random() % (IntTree::Size(tree) + 1));
+               }
+               while (tree != nullptr) {
+                 tree = Remove(tree, random() % (IntTree::Size(tree)));
+               }
+             }}};
+  }
+};
+
+template <>
+const bool tests::TestGroup<ConstTreeTests>::registration_ =
+    tests::Add<editor::ConstTreeTests>();
+}  // namespace
 }  // namespace afc::editor
