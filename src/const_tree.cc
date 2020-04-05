135d32d20bbd0deb5ac9ee9913716a3bbb27538b
Alejandro Forero Cuervo <alefore@gmail.com>
2020-04-05 02:13:29 +0200

    Add more benchmarks.

--- src/const_tree.cc
+++ src/const_tree.cc
@@ -36,22 +36,91 @@ bool registration_add =
 bool registration_prefix =
     tests::RegisterBenchmark(L"ConstTree::Prefix", [](int elements) {
       auto tree = GetTree(elements);
+      static const int kRuns = 1e5;
       auto start = Now();
-      tree = IntTree::Prefix(tree, elements - 1);
+      for (int i = 0; i < kRuns; i++) {
+        size_t position = random() % elements;
+        CHECK_EQ(IntTree::Size(IntTree::Prefix(tree, position)), position);
+      }
       auto end = Now();
-      CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements) - 1);
+      return SecondsBetween(start, end) / kRuns;
+    });
+
+bool registration_tree_insert =
+    tests::RegisterBenchmark(L"ConstTree::Insert", [](int elements) {
+      auto tree = GetTree(elements);
+      auto start = Now();
+      int position = random() % (elements + 1);
+      tree = Insert(tree, position);
+      CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements) + 1);
+      auto end = Now();
       return SecondsBetween(start, end);
     });
 
-bool registration_insert =
-    tests::RegisterBenchmark(L"ConstTree::Insert", [](int elements) {
-      auto tree = GetTree(elements);
-      int position = random() % (elements + 1);
+bool registration_vector_insert =
+    tests::RegisterBenchmark(L"Vector::Insert", [](int elements) {
+      std::vector<int> v(elements);
       auto start = Now();
-      tree = Insert(tree, position);
+      int position = random() % (elements + 1);
+      v.insert(v.begin() + position, 0);
+      CHECK_EQ(v.size(), static_cast<size_t>(elements) + 1);
       auto end = Now();
-      CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements) + 1);
       return SecondsBetween(start, end);
+    });
+
+bool registration_append =
+    tests::RegisterBenchmark(L"ConstTree::Append", [](int elements) {
+      if (elements < 8) return 0.0;
+      auto left = GetTree(random() % elements);
+      auto right = GetTree(elements - IntTree::Size(left));
+      static const int kRuns = 1e5;
+      auto start = Now();
+      for (int i = 0; i < kRuns; i++) {
+        auto tree = IntTree::Append(left, right);
+        CHECK_EQ(IntTree::Size(tree), static_cast<size_t>(elements));
+      }
+      auto end = Now();
+      return SecondsBetween(start, end) / kRuns;
+    });
+
+bool registration_vector_append =
+    tests::RegisterBenchmark(L"Vector::Append", [](int elements) {
+      if (elements < 8) return 0.0;
+      std::vector<int> left(random() % elements);
+      std::vector<int> right(elements - left.size());
+      static const int kRuns = 1e5;
+      auto start = Now();
+      for (int i = 0; i < kRuns; i++) {
+        auto output = left;
+        output.insert(output.end(), right.begin(), right.end());
+        CHECK_EQ(output.size(), static_cast<size_t>(elements));
+      }
+      auto end = Now();
+      return SecondsBetween(start, end) / kRuns;
+    });
+
+bool registration_get =
+    tests::RegisterBenchmark(L"ConstTree::Get", [](int elements) {
+      auto tree = GetTree(elements);
+      static const int kRuns = 1e5;
+      auto start = Now();
+      for (int i = 0; i < kRuns; i++) {
+        tree->Get(random() % elements);
+      }
+      auto end = Now();
+      return SecondsBetween(start, end) / kRuns;
+    });
+
+bool registration_vector_get =
+    tests::RegisterBenchmark(L"Vector::Get", [](int elements) {
+      std::vector<int> v(elements);
+      static const int kRuns = 1e5;
+      auto start = Now();
+      for (int i = 0; i < kRuns; i++) {
+        v.at(random() % elements);
+      }
+      auto end = Now();
+      return SecondsBetween(start, end) / kRuns;
     });
 
 IntTree::Ptr Remove(IntTree::Ptr tree, size_t position) {
