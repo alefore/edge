48306aa37a671001a303d6beaf5cc4ac2757d8cc
Alejandro Forero Cuervo <alefore@gmail.com>
2020-04-11 13:40:57 +0200

    Clean the pass-by-value semantics for Substring functions.

--- src/substring.h
+++ src/substring.h
@@ -16,24 +16,24 @@ using std::shared_ptr;
 // Equivalent to:
 //
 //     Substring(input, pos, input.size() - pos);
-shared_ptr<LazyString> Substring(const shared_ptr<LazyString>& input,
-                                 ColumnNumber column);
+std::shared_ptr<LazyString> Substring(std::shared_ptr<LazyString> input,
+                                      ColumnNumber column);
 
 // Returns the contents in [pos, pos + len).
 //
 // pos and len must be in the correct range (or else we'll crash).
 //
 // Example: Substring("alejo", 1, 2) := "le"
-shared_ptr<LazyString> Substring(const shared_ptr<LazyString>& input,
-                                 ColumnNumber column, ColumnNumberDelta delta);
+std::shared_ptr<LazyString> Substring(std::shared_ptr<LazyString> input,
+                                      ColumnNumber column,
+                                      ColumnNumberDelta delta);
 
 // Similar to the other versions, but performs checks on the bounds; instead of
 // crashing on invalid bounds, returns a shorter string.
 //
 // Example: Substring("carla", 2, 30) := "rla"
 std::shared_ptr<LazyString> SubstringWithRangeChecks(
-    const shared_ptr<LazyString>& input, ColumnNumber column,
-    ColumnNumberDelta delta);
+    shared_ptr<LazyString> input, ColumnNumber column, ColumnNumberDelta delta);
 
 }  // namespace editor
 }  // namespace afc
