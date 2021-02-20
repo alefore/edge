bcd9f77ca00090e486bf7a974a03b8d202571c76
Alejandro Forero Cuervo <alefore@gmail.com>
2021-02-20 14:22:46 +0100

    Add '/' support for 'ag', to filter buffers based on their contents.

--- src/set_buffer_mode.cc
+++ src/set_buffer_mode.cc
@@ -1,6 +1,7 @@
 #include "src/set_buffer_mode.h"
 
 #include "src/char_buffer.h"
+#include "src/search_handler.h"
 #include "src/tokenize.h"
 
 namespace afc::editor {
@@ -12,15 +13,20 @@ struct Operation {
     kNumber,
     kFilter,
     // Toggle WarningFilter: Only select buffers that have a warning status.
-    kWarningFilter
+    kWarningFilter,
+    // Toggle Search filter: Only select buffers that match a given regular
+    // expression.
+    kSearch,
   };
   Type type;
   size_t number = 0;
-  std::wstring filter = L"";
+  std::wstring text_input = L"";
 };
+
 struct Data {
-  enum class State { kDefault, kReadingFilter };
+  enum class State { kDefault, kReadingFilter, kReadingSearch };
   // If state is kReadingFilter, the back of operations must be of type kFilter.
+  // If state is kReadingSearch, the back of operations must be of type kSearch.
   State state = State::kDefault;
 
   std::vector<Operation> operations;
@@ -31,6 +37,9 @@ bool CharConsumer(wint_t c, Data* data) {
   CHECK(data->state != Data::State::kReadingFilter ||
         (!data->operations.empty() &&
          data->operations.back().type == Operation::Type::kFilter));
+  CHECK(data->state != Data::State::kReadingSearch ||
+        (!data->operations.empty() &&
+         data->operations.back().type == Operation::Type::kSearch));
   switch (data->state) {
     case Data::State::kDefault:
       // TODO: Get rid of this cast, ugh.
@@ -72,19 +81,27 @@ bool CharConsumer(wint_t c, Data* data) {
           data->state = Data::State::kReadingFilter;
           data->operations.push_back({Operation::Type::kFilter});
           return true;
+
+        case L'/':
+          data->state = Data::State::kReadingSearch;
+          data->operations.push_back({Operation::Type::kSearch});
+          return true;
+
         default:
           return false;
       }
 
     case Data::State::kReadingFilter:
+    case Data::State::kReadingSearch:
       switch (static_cast<int>(c)) {
         case L'\n':
+          // TODO: If text_input is empty, just pop it.
           data->state = Data::State::kDefault;
           return true;
         case Terminal::ESCAPE:
           return false;
         default:
-          data->operations.back().filter.push_back(c);
+          data->operations.back().text_input.push_back(c);
           return true;
       }
   }
@@ -109,7 +126,7 @@ std::wstring BuildStatus(const Data& data) {
         output += std::to_wstring(operation.number);
         break;
       case Operation::Type::kFilter:
-        output += L" w:" + operation.filter;
+        output += L" w:" + operation.text_input;
         if (i == data.operations.size() - 1 &&
             data.state == Data::State::kReadingFilter) {
           output += L"…";
@@ -118,6 +135,13 @@ std::wstring BuildStatus(const Data& data) {
       case Operation::Type::kWarningFilter:
         output += L" !";
         break;
+      case Operation::Type::kSearch:
+        output += L" /:" + operation.text_input;
+        if (i == data.operations.size() - 1 &&
+            data.state == Data::State::kReadingSearch) {
+          output += L"…";
+        }
+        break;
     }
   }
   return output;
@@ -125,13 +149,14 @@ std::wstring BuildStatus(const Data& data) {
 
 futures::Value<EmptyValue> Apply(EditorState* editor,
                                  CommandArgumentModeApplyMode mode, Data data) {
+  // Each entry is an index (e.g., for BuffersList::GetBuffer) for an
+  // available buffer.
+  using Indices = std::vector<size_t>;
   auto buffers_list = editor->buffer_tree();
 
-  // Each entry is an index (e.g., for BuffersList::GetBuffer) for an available
-  // buffer.
-  std::vector<size_t> indices(buffers_list->BuffersCount());
-  for (size_t i = 0; i < indices.size(); ++i) {
-    indices[i] = i;
+  Indices initial_indices(buffers_list->BuffersCount());
+  for (size_t i = 0; i < initial_indices.size(); ++i) {
+    initial_indices[i] = i;
   }
 
   bool warning_filter_enabled = false;
@@ -143,91 +168,171 @@ futures::Value<EmptyValue> Apply(EditorState* editor,
 
   if (warning_filter_enabled) {
     std::vector<size_t> new_indices;
-    for (auto& index : indices) {
+    for (auto& index : initial_indices) {
       if (auto buffer = buffers_list->GetBuffer(index).get();
           buffer->status()->GetType() == Status::Type::kWarning) {
         new_indices.push_back(index);
       }
     }
     if (new_indices.empty()) return futures::Past(EmptyValue());
-    indices = std::move(new_indices);
+    initial_indices = std::move(new_indices);
   }
 
-  // This is an index into `indices`.
-  size_t index = data.initial_number.value_or(buffers_list->GetCurrentIndex()) %
-                 indices.size();
+  struct State {
+    // This is an index into `indices`.
+
+    size_t index;
+    Indices indices;
+  };
+  futures::Value<State> state = futures::Past(State{
+      .index = data.initial_number.value_or(buffers_list->GetCurrentIndex()) %
+               initial_indices.size(),
+
+      .indices = std::move(initial_indices)});
   for (const auto& operation : data.operations) {
     switch (operation.type) {
       case Operation::Type::kForward:
-        index++;
+        state = futures::Transform(state, [](State state) {
+          state.index++;
+          return state;
+        });
         break;
 
       case Operation::Type::kBackward:
-        if (index == 0) {
-          index = indices.size() - 1;
-        } else {
-          index--;
-        }
+        state = futures::Transform(state, [](State state) {
+          if (state.indices.empty()) {
+            state.index = 0;
+          } else if (state.index == 0) {
+            state.index = state.indices.size() - 1;
+          } else {
+            state.index--;
+          }
+          return state;
+        });
         break;
 
       case Operation::Type::kNumber: {
         CHECK_GT(operation.number, 0ul);
-        int number_requested =
-            (operation.number - 1) % buffers_list->BuffersCount();
-        auto it = std::find_if(indices.begin(), indices.end(),
-                               [number_requested](int index) {
-                                 return index >= number_requested;
-                               });
-        index = it == indices.end() ? indices[0]
-                                    : std::distance(indices.begin(), it);
+        state = futures::Transform(
+            state,
+            [number_requested = (operation.number - 1) %
+                                buffers_list->BuffersCount()](State state) {
+              auto it = std::find_if(state.indices.begin(), state.indices.end(),
+                                     [number_requested](size_t index) {
+                                       return index >= number_requested;
+                                     });
+              state.index = it == state.indices.end()
+                                ? state.indices[0]
+                                : std::distance(state.indices.begin(), it);
+              return state;
+            });
       } break;
 
-      case Operation::Type::kFilter: {
-        std::vector<size_t> new_indices;
-        auto filter = TokenizeBySpaces(*NewLazyString(operation.filter));
-        for (auto& index : indices) {
-          if (auto buffer = buffers_list->GetBuffer(index).get();
-              buffer != nullptr) {
-            if (std::shared_ptr<LazyString> str =
-                    NewLazyString(buffer->Read(buffer_variables::name));
-                !FindFilterPositions(
-                     filter, ExtendTokensToEndOfString(
-                                 str, TokenizeNameForPrefixSearches(str)))
-                     .empty()) {
-              new_indices.push_back(index);
-            }
-          }
-        }
-        if (new_indices.empty()) return futures::Past(EmptyValue());
-        indices = std::move(new_indices);
-      } break;
+      case Operation::Type::kFilter:
+        state = futures::Transform(
+            state,
+            [filter = TokenizeBySpaces(*NewLazyString(operation.text_input)),
+             buffers_list](State state) {
+              std::vector<size_t> new_indices;
+              for (auto& index : state.indices) {
+                if (auto buffer = buffers_list->GetBuffer(index).get();
+                    buffer != nullptr) {
+                  if (std::shared_ptr<LazyString> str =
+                          NewLazyString(buffer->Read(buffer_variables::name));
+                      !FindFilterPositions(
+                           filter, ExtendTokensToEndOfString(
+                                       str, TokenizeNameForPrefixSearches(str)))
+                           .empty()) {
+                    new_indices.push_back(index);
+                  }
+                }
+              }
+              state.indices = std::move(new_indices);
+              return state;
+            });
+        break;
 
       case Operation::Type::kWarningFilter:
         break;  // Already handled.
+
+      case Operation::Type::kSearch: {
+        state = futures::Transform(state, [editor, buffers_list,
+                                           text_input = operation.text_input](
+                                              State state) {
+          // TODO: Maybe tweak the parameters to allow more than just one to
+          // run at a given time? Would require changes to async_processor.h
+          // (I think).
+          auto evaluator = std::make_shared<AsyncSearchProcessor>(
+              editor->work_queue(),
+              BackgroundCallbackRunner::Options::QueueBehavior::kWait);
+          auto progress_channel = std::make_shared<ProgressChannel>(
+              editor->work_queue(), [](ProgressInformation) {},
+              WorkQueueChannelConsumeMode::kLastAvailable);
+          auto new_state = std::make_shared<State>(
+              State{.index = state.index, .indices = {}});
+          std::vector<futures::Value<futures::IterationControlCommand>>
+              search_futures;
+          for (auto& index : state.indices) {
+            if (auto buffer = buffers_list->GetBuffer(index).get();
+                buffer != nullptr) {
+              // TODO: Pass SearchOptions::abort_notification to allow
+              // aborting as the user continues to type?
+              search_futures.push_back(futures::Transform(
+                  evaluator->Search(
+                      {.search_query = text_input, .required_positions = 1},
+                      *buffer, progress_channel),
+                  [new_state,
+                   index](AsyncSearchProcessor::Output search_output) {
+                    if (search_output.pattern_error) {
+                      // TODO: Communicate the error.
+                      return futures::IterationControlCommand::kStop;
+                    }
+                    if (search_output.matches > 0) {
+                      new_state->indices.push_back(index);
+                    }
+                    return futures::IterationControlCommand::kContinue;
+                  }));
+            }
+          }
+          return futures::Transform(
+              futures::ForEachWithCopy(
+                  search_futures.begin(), search_futures.end(),
+                  [](futures::Value<futures::IterationControlCommand> output) {
+                    return output;
+                  }),
+              [new_state](futures::IterationControlCommand) {
+                return std::move(*new_state);
+              });
+        });
+      } break;
     }
   }
-  CHECK(!indices.empty());
 
-  index %= indices.size();
-  editor->set_current_buffer(buffers_list->GetBuffer(indices[index]));
-  switch (mode) {
-    case CommandArgumentModeApplyMode::kFinal:
-      editor->buffer_tree()->set_filter(std::nullopt);
-      break;
+  return futures::Transform(state, [editor, mode, buffers_list](State state) {
+    if (state.indices.empty()) {
+      return EmptyValue();
+    }
+    state.index %= state.indices.size();
+    editor->set_current_buffer(
+        buffers_list->GetBuffer(state.indices[state.index]));
+    switch (mode) {
+      case CommandArgumentModeApplyMode::kFinal:
+        editor->buffer_tree()->set_filter(std::nullopt);
+        break;
 
-    case CommandArgumentModeApplyMode::kPreview:
-      if (indices.size() != buffers_list->BuffersCount()) {
-        std::vector<std::weak_ptr<OpenBuffer>> filter;
-        filter.reserve(indices.size());
-        for (const auto& i : indices) {
-          filter.push_back(buffers_list->GetBuffer(i));
+      case CommandArgumentModeApplyMode::kPreview:
+        if (state.indices.size() != buffers_list->BuffersCount()) {
+          std::vector<std::weak_ptr<OpenBuffer>> filter;
+          filter.reserve(state.indices.size());
+          for (const auto& i : state.indices) {
+            filter.push_back(buffers_list->GetBuffer(i));
+          }
+          editor->buffer_tree()->set_filter(std::move(filter));
         }
-        editor->buffer_tree()->set_filter(std::move(filter));
-      }
-      break;
-  }
-
-  return futures::Past(EmptyValue());
+        break;
+    }
+    return EmptyValue();
+  });
 }
 
 }  // namespace
