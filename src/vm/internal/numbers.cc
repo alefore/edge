6b1a044d46f428f30c3800d772a254fb6a2789fb
Alejandro Forero Cuervo <alefore@gmail.com>
2021-11-17 16:01:12 +0100

    Define a few additional number functions (log, exp...).

--- src/vm/internal/numbers.cc
+++ src/vm/internal/numbers.cc
@@ -7,6 +7,18 @@
 
 namespace afc::vm {
 void RegisterNumberFunctions(Environment* environment) {
+  environment->Define(L"log",
+                      vm::NewCallback(std::function<double(double)>(log),
+                                      VMType::PurityType::kPure));
+  environment->Define(L"log2",
+                      vm::NewCallback(std::function<double(double)>(log2),
+                                      VMType::PurityType::kPure));
+  environment->Define(L"log10",
+                      vm::NewCallback(std::function<double(double)>(log10),
+                                      VMType::PurityType::kPure));
+  environment->Define(L"exp",
+                      vm::NewCallback(std::function<double(double)>(exp),
+                                      VMType::PurityType::kPure));
   environment->Define(
       L"pow", vm::NewCallback(std::function<double(double, double)>(pow),
                               VMType::PurityType::kPure));
