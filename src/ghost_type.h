0a50a1ebabe8c1613dcd93b58899c057738387dc
Alejandro Forero Cuervo <alefore@gmail.com>
2022-01-29 17:27:10 +0100

    Add GHOST_TYPE_HASH* macros.

--- src/ghost_type.h
+++ src/ghost_type.h
@@ -1,6 +1,8 @@
 #ifndef __AFC_EDITOR_GHOST_TYPE_H__
 #define __AFC_EDITOR_GHOST_TYPE_H__
 
+#include <functional>
+
 namespace afc::editor {
 #define GHOST_TYPE_EQ(ClassName, variable)        \
   bool operator==(const ClassName& other) const { \
@@ -16,6 +18,17 @@ namespace afc::editor {
 #define GHOST_TYPE_BEGIN_END(ClassName, variable) \
   auto begin() const { return variable.begin(); } \
   auto end() const { return variable.end(); }
+
+#define GHOST_TYPE_HASH_FRIEND(ClassName, variable) \
+  friend class std::hash<ClassName>;
+
+#define GHOST_TYPE_HASH(ClassName, variable)              \
+  template <>                                             \
+  struct std::hash<ClassName> {                           \
+    std::size_t operator()(const ClassName& self) const { \
+      return std::hash<std::wstring>{}(self.variable);    \
+    }                                                     \
+  };
 
 }  // namespace afc::editor
 
