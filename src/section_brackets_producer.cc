460abcd717ae5025932cad1c905c8921891a609b
Alejandro Forero Cuervo <alefore@gmail.com>
2022-03-29 01:57:06 +0200

    Add tests and fix bug they uncover.

--- src/section_brackets_producer.cc
+++ src/section_brackets_producer.cc
@@ -10,22 +10,63 @@
 #include "src/char_buffer.h"
 #include "src/lazy_string.h"
 #include "src/line.h"
+#include "src/tests/tests.h"
 
 namespace afc::editor {
 LineWithCursor::Generator::Vector SectionBrackets(
     LineNumberDelta lines, SectionBracketsSide section_brackets_side) {
   LineWithCursor::Generator::Vector output{.lines = {},
                                            .width = ColumnNumberDelta(1)};
-  auto push = [&output](wstring c) {
-    output.lines.push_back(LineWithCursor::Generator{
-        std::hash<wstring>{}(c), [c]() {
-          return LineWithCursor(Line(Line::Options(NewLazyString(c))));
-        }});
+  auto push = [&output, lines](wstring c) {
+    if (output.size() < lines)
+      output.lines.push_back(LineWithCursor::Generator{
+          std::hash<wstring>{}(c), [c]() {
+            return LineWithCursor(Line(Line::Options(NewLazyString(c))));
+          }});
   };
   push(section_brackets_side == SectionBracketsSide::kLeft ? L"╭" : L"╮");
   for (LineNumberDelta i(1); i + LineNumberDelta(1) < lines; ++i) push(L"│");
   push(section_brackets_side == SectionBracketsSide::kLeft ? L"╰" : L"╯");
   push(L"");
   return output;
+}
+
+namespace {
+const bool tests_registration = tests::Register(
+    L"SectionBrackets",
+    {
+        {.name = L"Empty",
+         .callback =
+             [] {
+               CHECK_EQ(SectionBrackets(LineNumberDelta(0),
+                                        SectionBracketsSide::kLeft)
+                            .size(),
+                        LineNumberDelta(0));
+             }},
+        {.name = L"Short",
+         .callback =
+             [] {
+               CHECK_EQ(SectionBrackets(LineNumberDelta(1),
+                                        SectionBracketsSide::kLeft)
+                            .size(),
+                        LineNumberDelta(1));
+               CHECK_EQ(SectionBrackets(LineNumberDelta(2),
+                                        SectionBracketsSide::kLeft)
+                            .size(),
+                        LineNumberDelta(2));
+               CHECK_EQ(SectionBrackets(LineNumberDelta(3),
+                                        SectionBracketsSide::kLeft)
+                            .size(),
+                        LineNumberDelta(3));
+             }},
+        {.name = L"BasicCall",
+         .callback =
+             [] {
+               auto output = SectionBrackets(LineNumberDelta(10),
+                                             SectionBracketsSide::kLeft);
+               CHECK_EQ(output.size(), LineNumberDelta(10));
+               CHECK_EQ(output.width, ColumnNumberDelta(1));
+             }},
+    });
 }
 }  // namespace afc::editor
