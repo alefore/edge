94f2993a1e060b573a3f5d3ffa2e57b0652c887c
Alejandro Forero Cuervo <alefore@gmail.com>
2022-04-06 02:36:23 +0200

    Add const methods.

--- src/protected.h
+++ src/protected.h
@@ -6,6 +6,7 @@ template <typename T>
 class Protected {
  public:
   using Lock = std::unique_ptr<T, std::function<void(T*)>>;
+  using ConstLock = std::unique_ptr<const T, std::function<void(const T*)>>;
 
   Protected(T t) : data_(std::move(t)) {}
   Protected() = default;
@@ -17,14 +18,25 @@ class Protected {
     return Lock(&data_, [this](T*) { mutex_.unlock(); });
   }
 
+  ConstLock lock() const {
+    mutex_.lock();
+    return ConstLock(&data_, [this](const T*) { mutex_.unlock(); });
+  }
+
   template <typename Callable>
   auto lock(Callable callable) {
     std::unique_lock<std::mutex> lock(mutex_);
     return callable(data_);
   }
 
+  template <typename Callable>
+  auto lock(Callable callable) const {
+    std::unique_lock<std::mutex> lock(mutex_);
+    return callable(data_);
+  }
+
  private:
-  std::mutex mutex_;
+  mutable std::mutex mutex_;
   T data_;
 };
 }  // namespace afc::editor
