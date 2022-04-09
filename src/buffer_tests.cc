c4c4eb39ab81cd3b776cf1fbe0b5dac93f7763c1
Alejandro Forero Cuervo <alefore@gmail.com>
2022-04-09 15:10:14 +0200

    Add test for lines with existing metadata.

--- src/buffer_tests.cc
+++ src/buffer_tests.cc
@@ -2,6 +2,7 @@
 #include "src/buffer.h"
 #include "src/char_buffer.h"
 #include "src/editor.h"
+#include "src/safe_types.h"
 #include "src/tests/tests.h"
 
 namespace afc::editor {
@@ -21,42 +22,59 @@ std::wstring GetMetadata(std::wstring line) {
 
 const bool buffer_tests_registration = tests::Register(
     L"BufferTests",
-    {{.name = L"MetadataSimpleInt",
-      .callback = [] { CHECK(GetMetadata(L"5") == L"5"); }},
-     {.name = L"MetadataSimpleDouble",
-      .callback = [] { CHECK(GetMetadata(L"2.3") == L"2.3"); }},
-     {.name = L"MetadataSimpleString",
-      .callback = [] { CHECK(GetMetadata(L"\"xyz\"") == L"\"xyz\""); }},
-     {.name = L"MetadataSimpleExpression",
-      .callback = [] { CHECK(GetMetadata(L"1 + 2 + 3") == L"6"); }},
-     {.name = L"MetadataFunctionPure",
-      .callback =
-          [] {
-            CHECK(GetMetadata(L"[](int x) -> int { return x * 2; }(4)") ==
-                  L"8");
-          }},
-     {.name = L"MetadataImpureDoesNotExecute",
-      .callback =
-          [] {
-            CHECK(GetMetadata(L"buffer.SetStatus(\"xyz\"); 4") ==
-                  L"C++: \"int\"");
-          }},
-     {.name = L"MetadataPurePow",
-      .callback = [] { CHECK(GetMetadata(L"2 * pow(5, 3)") == L"250"); }},
-     {.name = L"MetadataScientificNotation",
-      .callback = [] { CHECK(GetMetadata(L"1e3") == L"1000"); }},
-     {.name = L"MetadataScientificNotationPlus",
-      .callback = [] { CHECK(GetMetadata(L"1e+3") == L"1000"); }},
-     {.name = L"MetadataScientificNotationMinus",
-      .callback = [] { CHECK(GetMetadata(L"1e-3") == L"0.001"); }},
-     {.name = L"MetadataIntToStringNormal",
-      .callback = [] { CHECK(GetMetadata(L"(1).tostring()") == L"\"1\""); }},
-     {.name = L"MetadataIntToStringRuntimeError",
-      .callback =
-          [] {
-            CHECK(GetMetadata(L"(1/0).tostring()").substr(0, 3) == L"E: ");
-          }}
-
+    {
+        {.name = L"MetadataSimpleInt",
+         .callback = [] { CHECK(GetMetadata(L"5") == L"5"); }},
+        {.name = L"MetadataSimpleDouble",
+         .callback = [] { CHECK(GetMetadata(L"2.3") == L"2.3"); }},
+        {.name = L"MetadataSimpleString",
+         .callback = [] { CHECK(GetMetadata(L"\"xyz\"") == L"\"xyz\""); }},
+        {.name = L"MetadataSimpleExpression",
+         .callback = [] { CHECK(GetMetadata(L"1 + 2 + 3") == L"6"); }},
+        {.name = L"MetadataFunctionPure",
+         .callback =
+             [] {
+               CHECK(GetMetadata(L"[](int x) -> int { return x * 2; }(4)") ==
+                     L"8");
+             }},
+        {.name = L"MetadataImpureDoesNotExecute",
+         .callback =
+             [] {
+               CHECK(GetMetadata(L"buffer.SetStatus(\"xyz\"); 4") ==
+                     L"C++: \"int\"");
+             }},
+        {.name = L"MetadataPurePow",
+         .callback = [] { CHECK(GetMetadata(L"2 * pow(5, 3)") == L"250"); }},
+        {.name = L"MetadataScientificNotation",
+         .callback = [] { CHECK(GetMetadata(L"1e3") == L"1000"); }},
+        {.name = L"MetadataScientificNotationPlus",
+         .callback = [] { CHECK(GetMetadata(L"1e+3") == L"1000"); }},
+        {.name = L"MetadataScientificNotationMinus",
+         .callback = [] { CHECK(GetMetadata(L"1e-3") == L"0.001"); }},
+        {.name = L"MetadataIntToStringNormal",
+         .callback = [] { CHECK(GetMetadata(L"(1).tostring()") == L"\"1\""); }},
+        {.name = L"MetadataIntToStringRuntimeError",
+         .callback =
+             [] {
+               CHECK(GetMetadata(L"(1/0).tostring()").substr(0, 3) == L"E: ");
+             }},
+        {.name = L"HonorsExistingMetadata",
+         .callback =
+             [] {
+               auto buffer = NewBufferForTests();
+               Line::Options options(NewLazyString(L"foo"));
+               options.SetMetadata(Line::MetadataEntry{
+                   .initial_value = NewLazyString(L"bar"),
+                   .value = futures::Past(
+                       std::shared_ptr<LazyString>(NewLazyString(L"quux")))});
+               buffer->AppendRawLine(
+                   std::make_shared<Line>(std::move(options)));
+               // Gives it a chance to execute:
+               buffer->editor().work_queue()->Execute();
+               CHECK(Pointer(buffer->contents().back()->metadata())
+                         .Reference()
+                         .ToString() == L"quux");
+             }},
     });
 
 const bool buffer_work_queue_tests_registration = tests::Register(
