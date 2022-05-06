9fbb51770b32d620929ed86ae33cf0ddd6cd37ea
Alejandro Forero Cuervo <alefore@gmail.com>
2022-05-06 11:09:57 +0200

    Fix use-after-move bug.

--- src/vm/internal/class_expression.cc
+++ src/vm/internal/class_expression.cc
@@ -106,7 +106,8 @@ void FinishClassDeclaration(
       });
   compilation->environment->DefineType(class_type.object_type,
                                        std::move(class_object_type));
-  auto constructor = Value::NewFunction(
+  auto purity = constructor_expression.value()->purity();
+  std::unique_ptr<Value> constructor = Value::NewFunction(
       {class_type},
       [constructor_expression_shared = NonNull<std::shared_ptr<Expression>>(
            std::move(constructor_expression.value())),
@@ -138,7 +139,7 @@ void FinishClassDeclaration(
               return error;
             });
       });
-  constructor->type.function_purity = constructor_expression.value()->purity();
+  constructor->type.function_purity = purity;
 
   compilation->environment->Define(class_type.object_type,
                                    std::move(constructor));
