89035c1db9c154c3e66665539e12af8680688c70
Alejandro Forero Cuervo <alefore@gmail.com>
2022-05-20 18:07:59 +0200

    Register convenience `Days` function.

--- rc/editor_commands/lib/dates.cc
+++ rc/editor_commands/lib/dates.cc
@@ -4,3 +4,10 @@ string Yesterday() { return FormatDay(Now().AddDays(-1)); }
 string Today() { return FormatDay(Now()); }
 string Tomorrow() { return FormatDay(Now().AddDays(1)); }
 string now() { return Now().format("%Y-%m-%d %H:%M"); }
+
+// Receives the dates as ISO yyyy-mm-dd strings and returns an approximation of
+// the number of days between them.
+double Days(string date_a, string date_b) {
+  return DurationBetween(ParseTime(date_a, "%F"), ParseTime(date_b, "%F"))
+      .days();
+}
