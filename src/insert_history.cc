12c914393521356d5726e5593423ba13236ddb7d
Alejandro Forero Cuervo <alefore@gmail.com>
2022-05-22 10:59:13 +0200

    Avoid inserting empty BufferContents.

--- src/insert_history.cc
+++ src/insert_history.cc
@@ -1,9 +1,14 @@
 #include "src/insert_history.h"
 
+#include "src/language/wstring.h"
+
 namespace afc::editor {
 using language::NonNull;
+using ::operator<<;
 
 void InsertHistory::Append(const BufferContents& insertion) {
+  if (insertion.range().IsEmpty()) return;
+  VLOG(5) << "Inserting to history: " << insertion.ToString();
   history_.push_back(insertion.copy());
 }
 
