00b2367ac1f1d88af044c615f26f783509de2fd2
Alejandro Forero Cuervo <alefore@gmail.com>
2022-05-29 13:03:18 +0200

    Add some documentation/example for how to define tests.

--- src/tests/tests.h
+++ src/tests/tests.h
@@ -1,3 +1,27 @@
+// Module to define unit tests.
+//
+// The unit tests are always built into the binary. To run them, the binary
+// should call the `Run` method.
+//
+// To register unit tests, a module does something like this:
+//
+// const bool bayes_sort_tests_registration = tests::Register(
+//     L"BayesSort",
+//     {{.name = L"EmptyHistoryAndFeatures",
+//       .callback =
+//           [] { CHECK_EQ(Sort(History({}), FeaturesSet({})).size(), 0ul); }},
+//      {.name = L"SortOk", .callback = ... },
+//      ...});
+//
+// It'll often be helpful to define helper functions, as in:
+//
+// const bool bayes_sort_tests_registration = tests::Register(
+//     L"BayesSort",
+//     [] {
+//       auto test = [](...) { ... return tests::Test(...); };
+//       return std::vector({
+//           test(...), test(...), ...});
+//     }());
 #ifndef __AFC_EDITOR_TESTS_TESTS_H__
 #define __AFC_EDITOR_TESTS_TESTS_H__
 
