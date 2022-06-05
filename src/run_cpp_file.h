ecfa71e0a9302b97e26724554f45f20c46ca8dce
Alejandro Forero Cuervo <alefore@gmail.com>
2022-06-05 18:06:31 +0200

    Remove RunCppFileHandler from header.

--- src/run_cpp_file.h
+++ src/run_cpp_file.h
@@ -3,18 +3,12 @@
 
 #include <memory>
 
-#include "src/futures/futures.h"
 #include "src/language/safe_types.h"
-#include "src/language/value_or_error.h"
 #include "src/lazy_string.h"
 
 namespace afc::editor {
 class EditorState;
 class Command;
-futures::Value<language::PossibleError> RunCppFileHandler(
-    EditorState& editor_state,
-    language::NonNull<std::shared_ptr<LazyString>> input);
-
 language::NonNull<std::unique_ptr<Command>> NewRunCppFileCommand(
     EditorState& editor_state);
 }  // namespace afc::editor
