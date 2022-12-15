0405010dd5810fa4eb7492bbfd8b5c2363584a3c
Alejandro Forero Cuervo <alefore@gmail.com>
2022-12-15 10:57:44 +0100

    Add exp2 function.

--- src/vm/internal/numbers.cc
+++ src/vm/internal/numbers.cc
@@ -16,6 +16,7 @@ void RegisterNumberFunctions(gc::Pool& pool, Environment& environment) {
   add(L"log2", log2);
   add(L"log10", log10);
   add(L"exp", exp);
+  add(L"exp2", exp2);
   environment.Define(L"pow",
                      NewCallback(pool, PurityType::kPure,
                                  std::function<double(double, double)>(pow)));
