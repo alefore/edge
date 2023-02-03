fcf84774039354f04af67942f5c8e453226d6397
Alejandro Forero Cuervo <alefore@gmail.com>
2023-02-03 11:47:42 +0100

    Add tests for GetImplicitPromotion.
    Fix one bug they uncovered.

--- src/vm/internal/types_promotion.cc
+++ src/vm/internal/types_promotion.cc
@@ -1,5 +1,9 @@
 #include "src/vm/internal/types_promotion.h"
 
+#include "src/tests/tests.h"
+#include "src/vm/public/callbacks.h"
+#include "src/vm/public/environment.h"
+
 namespace afc::vm {
 using language::NonNull;
 using language::Success;
@@ -28,6 +32,7 @@ PromotionCallback GetImplicitPromotion(Type original, Type desired) {
 
     PromotionCallback output_callback = GetImplicitPromotion(
         original_function->output.get(), desired_function->output.get());
+    if (output_callback == nullptr) return nullptr;
 
     std::vector<PromotionCallback> inputs_callbacks;
     for (size_t i = 0; i < original_function->inputs.size(); i++) {
@@ -73,5 +78,69 @@ PromotionCallback GetImplicitPromotion(Type original, Type desired) {
     };
   }
   return nullptr;
+}
+
+namespace {
+const bool tests_registration = tests::Register(
+    L"GetImplicitPromotion",
+    {
+        {.name = L"NoPromotion",
+         .callback =
+             [] {
+               CHECK(GetImplicitPromotion(types::String{}, types::Int{}) ==
+                     nullptr);
+             }},
+        {.name = L"IntToDouble",
+         .callback =
+             [] {
+               gc::Pool pool({});
+               PromotionCallback callback =
+                   GetImplicitPromotion(types::Int{}, types::Double{});
+               CHECK(callback != nullptr);
+               gc::Root<Value> output = callback(pool, Value::NewInt(pool, 5));
+               CHECK_EQ(output.ptr()->get_double(), 5.0);
+             }},
+        {.name = L"FunctionNoPromotion",
+         .callback =
+             [] {
+               // No promotion: the return type doesn't match (int and string).
+               std::vector<Type> inputs = {types::String(), types::Bool()};
+               CHECK(GetImplicitPromotion(
+                         types::Function{.output = Type{types::Int{}},
+                                         .inputs = inputs},
+                         types::Function{.output = Type{types::String{}},
+                                         .inputs = inputs}) == nullptr);
+             }},
+        {.name = L"FunctionReturnType",
+         .callback =
+             [] {
+               gc::Pool pool({});
+               std::vector<Type> inputs = {types::String(), types::Bool()};
+               gc::Root<Value> promoted_function = GetImplicitPromotion(
+                   types::Function{.output = Type{types::Int{}},
+                                   .inputs = inputs},
+                   types::Function{.output = Type{types::Double{}},
+                                   .inputs = inputs})(
+                   pool, vm::NewCallback(pool, PurityType::kUnknown,
+                                         [](std::wstring s, bool b) -> int {
+                                           CHECK(s == L"alejo");
+                                           CHECK_EQ(b, true);
+                                           return 4;
+                                         }));
+               Trampoline trampoline(Trampoline::Options{
+                   .pool = pool,
+                   .environment = Environment::NewDefault(pool),
+                   .yield_callback = nullptr});
+               futures::ValueOrError<EvaluationOutput> output =
+                   promoted_function.ptr()->LockCallback()(
+                       {Value::NewString(pool, L"alejo"),
+                        Value::NewBool(pool, true)},
+                       trampoline);
+               CHECK_EQ(std::get<EvaluationOutput>(output.Get().value())
+                            .value.ptr()
+                            ->get_double(),
+                        4.0);
+             }},
+    });
 }
 }  // namespace afc::vm
