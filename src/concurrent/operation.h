f9b45019579266216962725aaf80b81c92e6c694
Alejandro Forero Cuervo <alefore@gmail.com>
2023-04-06 08:21:54 +0200

    Add more comments and assertions. Use `unsigned` int.
    Avoid `notify_one` if there are still pending operations.

--- src/concurrent/operation.h
+++ src/concurrent/operation.h
@@ -7,13 +7,15 @@
 #include "src/concurrent/thread_pool.h"
 
 namespace afc::concurrent {
+// A barrier: blocks deletion until all callables given to `Operation::Add` have
+// successfully executed.
 class Operation {
  public:
   Operation(ThreadPool& thread_pool) : thread_pool_(thread_pool) {}
 
   ~Operation() {
     VLOG(5) << "Operation destruction.";
-    pending_operations_.wait([](int& i) {
+    pending_operations_.wait([](unsigned int& i) {
       VLOG(6) << "Checking operation with: " << i;
       return i == 0;
     });
@@ -22,27 +24,28 @@ class Operation {
 
   template <typename Callable>
   void Add(Callable callable) {
-    pending_operations_.lock([](int& i, std::condition_variable&) {
+    pending_operations_.lock([](unsigned int& i, std::condition_variable&) {
       i++;
       VLOG(7) << "Increment operations: " << i;
     });
-    thread_pool_.RunIgnoringResult(
-        [this, callable = std::make_shared<Callable>(std::move(callable))] {
-          VLOG(8) << "Running callable.";
-          (*callable)();
-          VLOG(9) << "Callable returned.";
-          pending_operations_.lock([](int& i, std::condition_variable& c) {
-            i--;
-            VLOG(7) << "Decremented operations: " << i;
-            c.notify_one();
-          });
-        });
+    thread_pool_.RunIgnoringResult([this, callable = std::make_shared<Callable>(
+                                              std::move(callable))] {
+      VLOG(8) << "Running callable.";
+      (*callable)();
+      VLOG(9) << "Callable returned.";
+      pending_operations_.lock([](unsigned int& i, std::condition_variable& c) {
+        CHECK_GT(i, 0ul);
+        i--;
+        VLOG(7) << "Decremented operations: " << i;
+        if (i == 0) c.notify_one();
+      });
+    });
   }
 
  private:
   ThreadPool& thread_pool_;
-  ProtectedWithCondition<int> pending_operations_ =
-      ProtectedWithCondition<int>(0);
+  ProtectedWithCondition<unsigned int> pending_operations_ =
+      ProtectedWithCondition<unsigned int>(0);
 };
 }  // namespace afc::concurrent
 
