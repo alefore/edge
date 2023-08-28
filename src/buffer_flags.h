0a96b2d545a26a7a7ee13adb7a51d89c61832dec
Alejandro Forero Cuervo <alefore@gmail.com>
2023-08-28 23:18:11 +0200

    Fix #includes.

--- src/buffer_flags.h
+++ src/buffer_flags.h
@@ -1,14 +1,10 @@
 #ifndef __AFC_EDITOR_BUFFER_FLAGS_H__
 #define __AFC_EDITOR_BUFFER_FLAGS_H__
 
-#include <map>
-#include <string>
 #include <vector>
 
 #include "src/buffer.h"
-#include "src/language/ghost_type.h"
-#include "src/language/wstring.h"
-#include "src/line_with_cursor.h"
+#include "src/infrastructure/screen/line_modifier.h"
 
 namespace afc::editor {
 std::vector<LineModifier> GetBufferFlag(const OpenBuffer& buffer);
