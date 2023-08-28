169635621132a31777d5f6c4ad44017cdbc9ebe3
Alejandro Forero Cuervo <alefore@gmail.com>
2023-08-28 21:04:41 +0200

    Fix `FilterOverlays`. Version from a previous commit was buggy.

--- src/buffer_output_producer.cc
+++ src/buffer_output_producer.cc
@@ -218,15 +218,17 @@ VisualOverlayMap FilterOverlays(const OpenBuffer& buffer,
   for (const std::pair<const VisualOverlayPriority,
                        std::map<VisualOverlayKey,
                                 std::multimap<LineColumn, VisualOverlay>>>&
-           priority_entry : buffer.visual_overlay_map())
+           priority_entry : buffer.visual_overlay_map()) {
+    DVLOG(5) << "Visiting overlay priority: " << priority_entry.first;
     for (const std::pair<const VisualOverlayKey,
                          std::multimap<LineColumn, VisualOverlay>>& key_entry :
          priority_entry.second) {
-      std::multimap<LineColumn, VisualOverlay> overlay_map;
-      if (auto overlay_it = overlay_map.lower_bound(screen_line_range.begin);
-          overlay_it != overlay_map.end() &&
+      DVLOG(5) << "Visiting overlay key: " << key_entry.first;
+      if (auto overlay_it =
+              key_entry.second.lower_bound(screen_line_range.begin);
+          overlay_it != key_entry.second.end() &&
           overlay_it->first < screen_line_range.end) {
-        while (overlay_it != overlay_map.end() &&
+        while (overlay_it != key_entry.second.end() &&
                overlay_it->first < screen_line_range.end) {
           CHECK_EQ(overlay_it->first.line, screen_line_range.end.line);
           CHECK_GE(overlay_it->first.column, screen_line_range.begin.column);
@@ -237,6 +239,8 @@ VisualOverlayMap FilterOverlays(const OpenBuffer& buffer,
         }
       }
     }
+  }
+  DVLOG(4) << "Output overlay priorities: " << output.size();
   return output;
 }
 }  // namespace
