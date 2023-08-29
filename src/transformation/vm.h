95b57a7981041d0e70dffd827d6e02ce2d54c1d6
Alejandro Forero Cuervo <alefore@gmail.com>
2023-08-29 20:54:05 +0200

    Fix includes and declarations.

--- src/transformation/vm.h
+++ src/transformation/vm.h
@@ -6,11 +6,18 @@
 #include <list>
 #include <memory>
 
+#include "src/language/safe_types.h"
+#include "src/transformation/type.h"
+#include "src/vm/public/callbacks.h"
+
 namespace afc::language::gc {
 class Pool;
 }
 namespace afc::vm {
 class Environment;
+template <>
+const types::ObjectName VMTypeMapper<language::NonNull<
+    std::shared_ptr<afc::editor::transformation::Variant>>>::object_type_name;
 }  // namespace afc::vm
 namespace afc::editor {
 void RegisterTransformations(language::gc::Pool& pool,
