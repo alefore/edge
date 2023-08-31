29e0509edc2515ed57831b3a4b9502b800ddfb3b
Alejandro Forero Cuervo <alefore@gmail.com>
2023-08-31 14:53:54 +0200

    Run git-commit+push on save if `.edge-git-push.txt` exists.

--- rc/hooks/buffer-save.cc
+++ rc/hooks/buffer-save.cc
@@ -1,8 +1,19 @@
+#include "../editor_commands/lib/paths.cc"
+
 string path = buffer.path();
 int dot = path.find_last_of(".", path.size());
 string extension = dot == -1 ? "" : path.substr(dot + 1, path.size() - dot - 1);
 
 ClangFormatOnSave();
+
+string git_push_path = Dirname(path) + "/.edge-git-push.txt";
+ForkCommandOptions git_push_options = ForkCommandOptions();
+git_push_options.set_command("test ! -f " + git_push_path.shell_escape() +
+                             " || ( git commit -a -m \"$( cat " +
+                             git_push_path.shell_escape() +
+                             ")\" && git push ) >/tmp/edge-git-push.log 2>&1");
+git_push_options.set_insertion_type("ignore");
+editor.ForkCommand(git_push_options);
 
 // if (path.starts_with("/home/alejo/edge/src")) {
 //   ForkCommandOptions options = ForkCommandOptions();
