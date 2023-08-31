68e58a30b129f563c73fc2d45c6206429573a339
Alejandro Forero Cuervo <alefore@gmail.com>
2023-08-31 22:39:18 +0200

    Add a few `using` declarations to simplify code.

--- src/language/lazy_string/functional.cc
+++ src/language/lazy_string/functional.cc
@@ -1,13 +1,14 @@
 #include "src/language/lazy_string/functional.h"
 
 namespace std {
+using afc::language::hash_combine;
+using afc::language::MakeHashableIteratorRange;
+
 std::size_t hash<afc::language::lazy_string::LazyString>::operator()(
     const afc::language::lazy_string::LazyString& input) const {
-  size_t value = 0;
-  afc::language::lazy_string::ForEachColumn(
-      input, [&](afc::language::lazy_string::ColumnNumber, wchar_t c) {
-        value = afc::language::hash_combine(value, c);
-      });
+  size_t value = 302948;
+  ForEachColumn(input, [&](afc::language::lazy_string::ColumnNumber,
+                           wchar_t c) { value = hash_combine(value, c); });
   return value;
 }
 }  // namespace std
