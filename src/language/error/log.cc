d1b7419f0159f22e32c14e0d5cb518a43ee3f42f
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-07 20:50:30 +0200

    Add tests and fix bug that they found.

--- src/language/error/log.cc
+++ src/language/error/log.cc
@@ -1,10 +1,12 @@
 #include "src/language/error/log.h"
 
+#include "src/tests/tests.h"
+
 namespace afc::language::error {
 Log::InsertResult Log::Insert(language::Error error,
                               infrastructure::Duration duration) {
   infrastructure::Time now = infrastructure::Now();
-  return entries_.lock([&](std::vector<ErrorAndExpiration> entries) {
+  return entries_.lock([&](std::vector<ErrorAndExpiration>& entries) {
     std::erase_if(entries, [now](const ErrorAndExpiration& entry) {
       return entry.expiration < now;
     });
@@ -18,4 +20,36 @@ Log::InsertResult Log::Insert(language::Error error,
     return output;
   });
 }
+
+namespace {
+const bool tests_registration = tests::Register(
+    L"error::Log",
+    {{.name = L"Creation", .callback = [] { Log(); }},
+     {.name = L"InsertIndependent",
+      .callback =
+          [] {
+            Log log;
+            CHECK(log.Insert(Error(L"Foo"), 1000) ==
+                  Log::InsertResult::kInserted);
+            CHECK(log.Insert(Error(L"Bar"), 1000) ==
+                  Log::InsertResult::kInserted);
+          }},
+     {.name = L"InsertFinds",
+      .callback =
+          [] {
+            Log log;
+            CHECK(log.Insert(Error(L"Foo"), 1000) ==
+                  Log::InsertResult::kInserted);
+            CHECK(log.Insert(Error(L"Foo"), 1000) ==
+                  Log::InsertResult::kAlreadyFound);
+          }},
+     {.name = L"InsertExpires", .callback = [] {
+        Log log;
+        CHECK(log.Insert(Error(L"Foo"), 1) == Log::InsertResult::kInserted);
+        CHECK(log.Insert(Error(L"Foo"), 1) == Log::InsertResult::kAlreadyFound);
+        sleep(2);
+        CHECK(log.Insert(Error(L"Foo"), 1) == Log::InsertResult::kInserted);
+      }}});
+}
+
 }  // namespace afc::language::error
