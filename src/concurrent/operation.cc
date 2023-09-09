66e2fc54b51a393d60cc7b9362227c6d20fb90ed
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-09 15:37:32 +0200

    Improve test for Concurrent::Operation to actually sleep.

--- src/concurrent/operation.cc
+++ src/concurrent/operation.cc
@@ -6,24 +6,24 @@
 namespace afc::concurrent {
 namespace {
 const bool tests_registration = tests::Register(
-    L"Concurrent::Operation", {{.name = L"Empty",
-                                .callback =
-                                    [] {
-                                      ThreadPool thread_pool(5, nullptr);
-                                      Operation op(thread_pool);
-                                    }},
-                               {.name = L"Sleeps", .callback = [] {
-                                  ThreadPool thread_pool(4, nullptr);
-                                  Protected<int> executions(0);
-                                  auto op =
-                                      std::make_unique<Operation>(thread_pool);
-                                  for (size_t i = 0; i < 8; i++)
-                                    op->Add([&] {
-                                      sleep(0.5);
-                                      (*executions.lock())++;
-                                    });
-                                  op = nullptr;
-                                  CHECK_EQ(*executions.lock(), 8);
-                                }}});
+    L"Concurrent::Operation",
+    {{.name = L"Empty",
+      .callback =
+          [] {
+            ThreadPool thread_pool(5, nullptr);
+            Operation op(thread_pool);
+          }},
+     {.name = L"Sleeps", .callback = [] {
+        ThreadPool thread_pool(4, nullptr);
+        Protected<int> executions(0);
+        auto op = std::make_unique<Operation>(thread_pool);
+        for (size_t i = 0; i < 8; i++)
+          op->Add([&] {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            (*executions.lock())++;
+          });
+        op = nullptr;
+        CHECK_EQ(*executions.lock(), 8);
+      }}});
 }
 }  // namespace afc::concurrent
