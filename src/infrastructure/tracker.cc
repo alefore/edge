adde25f29df922345cd4de353c1aa9f05c398c7e
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-10 10:44:48 +0200

    Avoid unnecessary capture.

--- src/infrastructure/tracker.cc
+++ src/infrastructure/tracker.cc
@@ -43,7 +43,7 @@ std::unique_ptr<bool, std::function<void(bool*)>> Tracker::Call() {
       new bool(), [this, start](bool* value) {
         double seconds = GetElapsedSecondsSince(start);
         delete value;
-        data_.lock([start, seconds](Data& data) {
+        data_.lock([seconds](Data& data) {
           data.seconds += seconds;
           data.longest_seconds = std::max(data.longest_seconds, seconds);
         });
