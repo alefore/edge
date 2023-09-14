a7db0099b74f0500c96c7b309392929d666f177c
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-14 23:43:30 +0200

    Add a way to limit concurrency.

--- src/concurrent/operation.h
+++ src/concurrent/operation.h
@@ -5,13 +5,16 @@
 
 #include "src/concurrent/protected.h"
 #include "src/concurrent/thread_pool.h"
+#include "src/language/safe_types.h"
 
 namespace afc::concurrent {
 // A barrier: blocks deletion until all callables given to `Operation::Add` have
 // successfully executed.
 class Operation {
  public:
-  Operation(ThreadPool& thread_pool) : thread_pool_(thread_pool) {}
+  Operation(ThreadPool& thread_pool,
+            std::optional<size_t> concurrency_limit = std::nullopt)
+      : thread_pool_(thread_pool), concurrency_limit_(concurrency_limit) {}
 
   ~Operation() {
     VLOG(5) << "Operation destruction.";
@@ -24,26 +27,48 @@ class Operation {
 
   template <typename Callable>
   void Add(Callable callable) {
-    pending_operations_.lock([](unsigned int& i, std::condition_variable&) {
-      i++;
-      VLOG(7) << "Increment operations: " << i;
-    });
-    thread_pool_.RunIgnoringResult([this, callable = std::make_shared<Callable>(
-                                              std::move(callable))] {
-      VLOG(8) << "Running callable.";
-      (*callable)();
-      VLOG(9) << "Callable returned.";
-      pending_operations_.lock([](unsigned int& i, std::condition_variable& c) {
-        CHECK_GT(i, 0ul);
-        i--;
-        VLOG(7) << "Decremented operations: " << i;
-        if (i == 0) c.notify_one();
-      });
-    });
+    LockSlot();
+    thread_pool_.RunIgnoringResult(
+        [this, callable = std::make_shared<Callable>(std::move(callable))] {
+          VLOG(8) << "Running callable.";
+          (*callable)();
+          VLOG(9) << "Callable returned.";
+          pending_operations_.lock(
+              [this](unsigned int& i, std::condition_variable& c) {
+                CHECK_GT(i, 0ul);
+                if (concurrency_limit_.has_value())
+                  CHECK_LE(i, concurrency_limit_.value());
+                i--;
+                VLOG(7) << "Decremented operations: " << i;
+                if (i == 0) c.notify_one();
+              });
+        });
   }
 
  private:
+  void LockSlot() {
+    language::VisitOptional(
+        [this](size_t limit) {
+          pending_operations_.wait([limit](unsigned int& i) {
+            CHECK_LE(i, limit);
+            VLOG(6) << "Checking operation with: " << i;
+            if (i == limit) return false;
+            i++;
+            return true;
+          });
+        },
+        [this] {
+          pending_operations_.lock(
+              [](unsigned int& i, std::condition_variable&) {
+                i++;
+                VLOG(7) << "Increment operations: " << i;
+              });
+        },
+        concurrency_limit_);
+  }
+
   ThreadPool& thread_pool_;
+  const std::optional<size_t> concurrency_limit_;
   ProtectedWithCondition<unsigned int> pending_operations_ =
       ProtectedWithCondition<unsigned int>(0);
 };
