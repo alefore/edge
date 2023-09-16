822a47cec10e3291f53f603536a81ba8c54eaa6e
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-16 15:39:28 +0200

    Change output format of Buffer::ShowTrackers: use CSV rows.

--- src/buffer_vm.cc
+++ src/buffer_vm.cc
@@ -378,13 +378,15 @@ gc::Root<ObjectType> BuildBufferType(gc::Pool& pool) {
           pool, vm::PurityTypeWriter,
           [](gc::Root<OpenBuffer> buffer) {
             for (auto& data : Tracker::GetData()) {
-              buffer.ptr()->AppendLine(
-                  Append(Append(NewLazyString(data.name), NewLazyString(L": "),
-                                NewLazyString(std::to_wstring(data.executions)),
-                                NewLazyString(L" ")),
-                         NewLazyString(std::to_wstring(data.seconds)),
-                         NewLazyString(L" "),
-                         NewLazyString(std::to_wstring(data.longest_seconds))));
+              buffer.ptr()->AppendLine(Append(
+                  Append(NewLazyString(L"\""), NewLazyString(data.name),
+                         NewLazyString(L"\","),
+                         NewLazyString(std::to_wstring(data.executions))),
+                  Append(
+                      NewLazyString(L","),
+                      NewLazyString(std::to_wstring(data.seconds)),
+                      NewLazyString(L","),
+                      NewLazyString(std::to_wstring(data.longest_seconds)))));
             }
           })
           .ptr());
