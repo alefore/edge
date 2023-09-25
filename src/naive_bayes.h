e22822a607f1c9585d494aedfddc122356fdf614
Alejandro Forero Cuervo <alefore@gmail.com>
2023-09-25 17:33:46 +0200

    Better descriptions.

--- src/naive_bayes.h
+++ src/naive_bayes.h
@@ -29,7 +29,7 @@ GHOST_TYPE_HASH(afc::naive_bayes::Feature);
 namespace afc::naive_bayes {
 
 // FeaturesSet represents a set of features. Typically this is used to capture
-// the state of an instance when an event was executed.
+// the state of the environment in which an event is executed.
 GHOST_TYPE_CONTAINER(FeaturesSet, std::unordered_set<Feature>);
 
 // The history represents all the past executions of all events. For each
@@ -37,8 +37,8 @@ GHOST_TYPE_CONTAINER(FeaturesSet, std::unordered_set<Feature>);
 using InternalHistoryType = std::unordered_map<Event, std::vector<FeaturesSet>>;
 GHOST_TYPE_CONTAINER(History, InternalHistoryType);
 
-// Given the history of all past executions of all events, apply Naive Bayes and
-// return the list of all keys, sorted by their predicted proportional
+// Given the history of all past executions of all events and the current state,
+// apply Naive Bayes to sort all events by their predicted proportional
 // probability (in ascending order).
 //
 // The returned vector contains the keys of `history`.
