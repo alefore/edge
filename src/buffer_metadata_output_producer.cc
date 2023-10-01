3f5a7f16d35890b1e697208d217b72e8e621fdfe
Alejandro Forero Cuervo <alefore@gmail.com>
2023-10-01 16:37:45 +0200

    Avoid dangling-reference warning.
    I think this is a false positive, but I suppose theoretically it _could_
    eventually have race conditions that make it fire.

--- src/buffer_metadata_output_producer.cc
+++ src/buffer_metadata_output_producer.cc
@@ -386,12 +386,13 @@ std::list<MetadataLine> Prepare(const BufferMetadataOutputOptions& options,
   auto top_call = top_tracker.Call();
 
   std::list<MetadataLine> output;
-  const Line& contents = options.buffer.contents().at(range.begin.line).value();
+  NonNull<std::shared_ptr<const Line>> contents =
+      options.buffer.contents().at(range.begin.line);
   std::optional<gc::Root<OpenBuffer>> target_buffer_dummy;
   NonNull<const OpenBuffer*> target_buffer =
       NonNull<const OpenBuffer*>::AddressOf(options.buffer);
   VisitPointer(
-      contents.outgoing_link(),
+      contents->outgoing_link(),
       [&](const OutgoingLink& link) {
         if (auto it =
                 options.buffer.editor().buffers()->find(BufferName(link.path));
@@ -422,7 +423,7 @@ std::list<MetadataLine> Prepare(const BufferMetadataOutputOptions& options,
   }
 
   VisitPointer(
-      contents.metadata(),
+      contents->metadata(),
       [&output](NonNull<std::shared_ptr<LazyString>> metadata) {
         static Tracker tracker(
             L"BufferMetadataOutput::Prepare:VisitContentsMetadata");
