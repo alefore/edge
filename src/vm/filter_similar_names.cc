7a6c3dc73366f1576f459c81cc9888df96bb1d80
Alejandro Forero Cuervo <alefore@gmail.com>
2023-10-11 17:40:12 +0200

    Add TODO.

--- src/vm/filter_similar_names.cc
+++ src/vm/filter_similar_names.cc
@@ -6,6 +6,7 @@ namespace afc::vm {
 std::vector<std::wstring> FilterSimilarNames(
     std::wstring name, std::vector<std::wstring> candidates) {
   // TODO(easy, 2022-11-26): This can be significantly more flexible.
+  // TODO(trivial, P0): Doesn't this have a bug? It should actually remove!
   std::remove_if(
       candidates.begin(), candidates.end(), [&name](std::wstring candidate) {
         return !std::equal(
