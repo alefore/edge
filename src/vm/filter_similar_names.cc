a22bac9d6b778489c05fe29f47eb62fa7595d647
Alejandro Forero Cuervo <alefore@gmail.com>
2023-10-11 17:45:07 +0200

    Fix bug in std::remove_if invocation in FilterSimilarNames.

--- src/vm/filter_similar_names.cc
+++ src/vm/filter_similar_names.cc
@@ -6,13 +6,16 @@ namespace afc::vm {
 std::vector<std::wstring> FilterSimilarNames(
     std::wstring name, std::vector<std::wstring> candidates) {
   // TODO(easy, 2022-11-26): This can be significantly more flexible.
-  // TODO(trivial, P0): Doesn't this have a bug? It should actually remove!
-  std::remove_if(
-      candidates.begin(), candidates.end(), [&name](std::wstring candidate) {
-        return !std::equal(
-            name.begin(), name.end(), candidate.begin(),
-            [](wchar_t a, wchar_t b) { return tolower(a) == tolower(b); });
-      });
+  candidates.erase(std::remove_if(candidates.begin(), candidates.end(),
+                                  [&name](std::wstring candidate) {
+                                    return !std::equal(
+                                        name.begin(), name.end(),
+                                        candidate.begin(),
+                                        [](wchar_t a, wchar_t b) {
+                                          return tolower(a) == tolower(b);
+                                        });
+                                  }),
+                   candidates.end());
   return candidates;
 }
 }  // namespace afc::vm
