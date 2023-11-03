e99c1356fd4c217f4fa2f5915a478f369e1e449e
Alejandro Forero Cuervo <alefore@gmail.com>
2023-11-03 14:01:28 +0100

    Add tests for futures::Serializer.
    Fix bug that they uncover.

--- src/futures/serializer.cc
+++ src/futures/serializer.cc
@@ -3,14 +3,96 @@
 #include "glog/logging.h"
 #include "src/futures/futures.h"
 #include "src/language/error/value_or_error.h"
+#include "src/tests/tests.h"
+
+using afc::language::EmptyValue;
 
 namespace afc::futures {
 void Serializer::Push(Callback callback) {
+  futures::Future<language::EmptyValue> new_future;
   auto last_execution = std::move(last_execution_);
-  last_execution_ =
-      std::move(last_execution)
-          .Transform([callback = std::move(callback)](language::EmptyValue) {
-            return callback();
+  last_execution_ = std::move(new_future.value);
+  std::move(last_execution)
+      .SetConsumer(
+          [callback = std::move(callback),
+           consumer = std::move(new_future.consumer)](language::EmptyValue) {
+            callback().SetConsumer(consumer);
           });
+}
+
+namespace {
+const bool tests_registration = tests::Register(
+    L"FuturesSerializer",
+    {{.name = L"Empty", .callback = [] { Serializer(); }},
+     {.name = L"Sync",
+      .callback =
+          [] {
+            Serializer serializer;
+            std::vector<size_t> calls;
+            for (size_t i = 0; i < 5; i++)
+              serializer.Push([&calls, i] {
+                calls.push_back(i);
+                return futures::Past(EmptyValue());
+              });
+            CHECK(calls == std::vector<size_t>({0, 1, 2, 3, 4}));
+          }},
+     {.name = L"Async",
+      .callback =
+          [] {
+            Serializer serializer;
+            std::vector<size_t> calls;
+            std::vector<Future<EmptyValue>> futures(6);
+            for (size_t i = 0; i < futures.size(); i++)
+              serializer.Push([&calls, &futures, i] {
+                calls.push_back(i);
+                return std::move(futures[i].value);
+              });
+            CHECK(calls == std::vector<size_t>({0}));
+            futures[0].consumer(EmptyValue());
+            CHECK(calls == std::vector<size_t>({0, 1}));
+            futures[2].consumer(EmptyValue());
+            futures[3].consumer(EmptyValue());
+            CHECK(calls == std::vector<size_t>({0, 1}));
+            futures[1].consumer(EmptyValue());
+            CHECK(calls == std::vector<size_t>({0, 1, 2, 3, 4}));
+            futures[4].consumer(EmptyValue());
+            CHECK(calls == std::vector<size_t>({0, 1, 2, 3, 4, 5}));
+            serializer.Push([&calls] {
+              calls.push_back(6);
+              return futures::Past(EmptyValue());
+            });
+            CHECK(calls == std::vector<size_t>({0, 1, 2, 3, 4, 5}));
+            futures[5].consumer(EmptyValue());
+            CHECK(calls == std::vector<size_t>({0, 1, 2, 3, 4, 5, 6}));
+          }},
+     {.name = L"Reentrant", .callback = [] {
+        Serializer serializer;
+        std::vector<Future<EmptyValue>> futures(4);
+        std::vector<size_t> calls;
+        serializer.Push([&] {
+          calls.push_back(0);
+          serializer.Push([&] {
+            calls.push_back(1);
+            return std::move(futures[1].value);
+          });
+          return std::move(futures[0].value);
+        });
+        serializer.Push([&] {
+          calls.push_back(2);
+          serializer.Push([&] {
+            calls.push_back(3);
+            return std::move(futures[3].value);
+          });
+          return std::move(futures[2].value);
+        });
+        CHECK(calls == std::vector<size_t>({0}));
+        futures[0].consumer(EmptyValue());
+        CHECK(calls == std::vector<size_t>({0, 1}));
+        futures[1].consumer(EmptyValue());
+        CHECK(calls == std::vector<size_t>({0, 1, 2}));
+        futures[2].consumer(EmptyValue());
+        CHECK(calls == std::vector<size_t>({0, 1, 2, 3}));
+        futures[3].consumer(EmptyValue());
+      }}});
 }
 }  // namespace afc::futures
