06216c940a2c669ec79a2ec05e9e9db30c13d105
Alejandro Forero Cuervo <alefore@gmail.com>
2023-11-06 21:41:31 +0100

    Add unit tests for KeyCommandsMap.

--- src/key_commands_map.cc
+++ src/key_commands_map.cc
@@ -10,6 +10,7 @@
 #include "src/language/text/line.h"
 #include "src/language/text/line_sequence.h"
 #include "src/language/text/mutable_line_sequence.h"
+#include "src/tests/tests.h"
 
 using afc::infrastructure::screen::LineModifier;
 using afc::infrastructure::screen::LineModifierSet;
@@ -132,4 +133,189 @@ LineSequence KeyCommandsMapSequence::Help() const {
   return help_output.snapshot();
 }
 
+namespace {
+const bool key_commands_map_tests_registration = tests::Register(
+    L"KeyCommandsMap",
+    {{.name = L"ExecuteReturnsFalseIfNotRegistered",
+      .callback = [] { CHECK(!KeyCommandsMap().Execute(L'x')); }},
+     {.name = L"Insert",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            bool executed = false;
+            map.Insert(L'a',
+                       {.category = KeyCommandsMap::Category::kStringControl,
+                        .description = Description(L"Test"),
+                        .handler = [&executed](wchar_t) {
+                          CHECK(!executed);
+                          executed = true;
+                        }});
+            CHECK(map.Execute(L'a'));
+            CHECK(executed);
+          }},
+     {.name = L"Erase",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            bool executed = false;
+            map.Insert(L'b',
+                       {.category = KeyCommandsMap::Category::kStringControl,
+                        .description = Description(L"Test"),
+                        .handler = [&executed](wchar_t) { executed = true; }});
+            map.Erase(L'b');
+            CHECK(!map.Execute(L'b'));
+            CHECK(!executed);
+          }},
+     {.name = L"FallbackFunctionality",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            bool fallback_executed = false;
+            map.SetFallback({}, [&fallback_executed](wchar_t) {
+              CHECK(!fallback_executed);
+              fallback_executed = true;
+            });
+            CHECK(map.Execute(L'x'));
+            CHECK(fallback_executed);
+          }},
+     {.name = L"FallbackExclusion",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            bool fallback_executed = false;
+            std::set<wchar_t> exclude = {L'y'};
+            map.SetFallback(exclude, [&fallback_executed](wchar_t) {
+              fallback_executed = true;
+            });
+            CHECK(!map.Execute(L'y'));
+            CHECK(!fallback_executed);
+          }},
+     {.name = L"FindCallbackNullForUnregistered",
+      .callback =
+          [] { CHECK(KeyCommandsMap().FindCallbackOrNull(L'z') == nullptr); }},
+     {.name = L"FindCallbackNotNull",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            map.Insert(L'c',
+                       {.category = KeyCommandsMap::Category::kDirection,
+                        .description = Description(L"Test callback"),
+                        .handler = [](wchar_t) { /* Handler code here */ }});
+            CHECK(map.FindCallbackOrNull(L'c') != nullptr);
+          }},
+     {.name = L"OnHandleExecution",
+      .callback =
+          [] {
+            size_t on_handle_executions = 0;
+            KeyCommandsMap map;
+            map.OnHandle([&on_handle_executions] { on_handle_executions++; })
+                .Insert(L'd',
+                        {.category = KeyCommandsMap::Category::kStructure,
+                         .description = Description(L"OnHandle test"),
+                         .handler = [](wchar_t) { /* Handler code here */ }});
+            for (size_t i = 0; i < 5; i++) {
+              CHECK_EQ(on_handle_executions, i);
+              map.Execute(L'd');
+              CHECK_EQ(on_handle_executions, i + 1);
+            }
+          }},
+     {.name = L"OnHandleNotRunForNotFoundCommand",
+      .callback =
+          [] {
+            size_t on_handle_executions = 0;
+            KeyCommandsMap map;
+            map.OnHandle([&on_handle_executions] { on_handle_executions++; });
+            map.Execute(L'e');  // Try executing an unregistered command.
+            CHECK_EQ(on_handle_executions, 0ul);
+          }},
+     {.name = L"OnHandleNotRunForExcludedFallback",
+      .callback =
+          [] {
+            size_t on_handle_executions = 0;
+            KeyCommandsMap map;
+            std::set<wchar_t> exclude = {L'f'};
+            map.SetFallback(exclude, [](wchar_t) { /* Fallback handler */ })
+                .OnHandle([&on_handle_executions] { on_handle_executions++; });
+            map.Execute(L'f');  // Try executing an excluded command.
+            CHECK_EQ(on_handle_executions, 0ul);
+          }},
+     {.name = L"OnHandleRunsForFallback",
+      .callback =
+          [] {
+            size_t on_handle_executions = 0;
+            KeyCommandsMap map;
+            map.SetFallback({}, [](wchar_t) { /* Fallback handler */ })
+                .OnHandle([&on_handle_executions] { on_handle_executions++; });
+            map.Execute(L'g');  // Trigger fallback execution.
+            CHECK_EQ(on_handle_executions, 1ul);
+          }},
+     {.name = L"ExecuteSpecificHandlerOnly",
+      .callback =
+          [] {
+            KeyCommandsMap map;
+            size_t execution_count[3] = {0, 0, 0};
+
+            map.Insert(L'0',
+                       {.category = KeyCommandsMap::Category::kStringControl,
+                        .description = Description(L"Execute0"),
+                        .handler = [&](wchar_t) { execution_count[0]++; }})
+                .Insert(L'1',
+                        {.category = KeyCommandsMap::Category::kRepetitions,
+                         .description = Description(L"Execute1"),
+                         .handler = [&](wchar_t) { execution_count[1]++; }})
+                .Insert(L'2',
+                        {.category = KeyCommandsMap::Category::kDirection,
+                         .description = Description(L"Execute2"),
+                         .handler = [&](wchar_t) { execution_count[2]++; }});
+
+            map.Execute(L'0');
+            CHECK_EQ(execution_count[0], 1ul);
+            CHECK_EQ(execution_count[1], 0ul);
+            CHECK_EQ(execution_count[2], 0ul);
+
+            map.Execute(L'1');
+            CHECK_EQ(execution_count[0], 1ul);
+            CHECK_EQ(execution_count[1], 1ul);
+            CHECK_EQ(execution_count[2], 0ul);
+
+            map.Execute(L'2');
+            CHECK_EQ(execution_count[0], 1ul);
+            CHECK_EQ(execution_count[1], 1ul);
+            CHECK_EQ(execution_count[2], 1ul);
+          }},
+     {.name = L"HandlerParameterCheck", .callback = [] {
+        KeyCommandsMap map;
+        size_t executions = 0;
+
+        map.Insert(L'0', {.category = KeyCommandsMap::Category::kStringControl,
+                          .description = Description(L"Handler for '0'"),
+                          .handler =
+                              [&executions](wchar_t c) {
+                                CHECK(c == L'0');
+                                executions++;
+                              }})
+            .Insert(L'1', {.category = KeyCommandsMap::Category::kRepetitions,
+                           .description = Description(L"Handler for '1'"),
+                           .handler =
+                               [&executions](wchar_t c) {
+                                 CHECK(c == L'1');
+                                 executions++;
+                               }})
+            .Insert(L'2', {.category = KeyCommandsMap::Category::kDirection,
+                           .description = Description(L"Handler for '2'"),
+                           .handler = [&executions](wchar_t c) {
+                             CHECK(c == L'2');
+                             executions++;
+                           }});
+
+        map.Execute(L'0');
+        CHECK_EQ(executions, 1ul);
+
+        map.Execute(L'1');
+        CHECK_EQ(executions, 2ul);
+
+        map.Execute(L'2');
+        CHECK_EQ(executions, 3ul);
+      }}});
+}  // namespace
 }  // namespace afc::editor::operation
