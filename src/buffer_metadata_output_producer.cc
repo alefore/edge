4960754f074d5a3fb6cc226d1d83d5c7279e83ab
Alejandro Forero Cuervo <alefore@gmail.com>
2023-12-04 18:11:20 +0100

    Fix recently introduced negation.
    This was a bug from 6b90dca.

--- src/buffer_metadata_output_producer.cc
+++ src/buffer_metadata_output_producer.cc
@@ -483,7 +483,7 @@ std::list<MetadataLine> Prepare(const BufferMetadataOutputOptions& options,
         L"BufferMetadataOutput::Prepare:AddMetadataForExpiredMark");
     auto call = tracker.Call();
     if (auto mark_contents = mark.source_line_content->ToString();
-        marks_strings.contains(mark_contents)) {
+        !marks_strings.contains(mark_contents)) {
       output.push_back(
           MetadataLine{'!', LineModifier::kRed,
                        MakeNonNullShared<Line>(L"👻 " + mark_contents),
