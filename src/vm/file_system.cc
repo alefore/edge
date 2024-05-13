fc516019135fcd10ef9a086ec040e83f1be81d2e
Alejandro Forero Cuervo <alefore@gmail.com>
2024-05-13 12:13:25 +0200

    vm: Expose Glob function.

--- src/vm/file_system.cc
+++ src/vm/file_system.cc
@@ -2,11 +2,18 @@
 
 #include "src/infrastructure/dirname.h"
 #include "src/infrastructure/dirname_vm.h"
+#include "src/language/container.h"
+#include "src/language/error/value_or_error.h"
 #include "src/vm/callbacks.h"
+#include "src/vm/container.h"
+#include "src/vm/string.h"
 
 using afc::infrastructure::FileSystemDriver;
 using afc::infrastructure::Path;
 using afc::language::EmptyValue;
+using afc::language::MakeNonNullShared;
+using afc::language::NonNull;
+using afc::language::lazy_string::LazyString;
 
 namespace afc::vm {
 void RegisterFileSystemFunctions(
@@ -20,5 +27,24 @@ void RegisterFileSystemFunctions(
                           -> futures::ValueOrError<EmptyValue> {
                         return file_system_driver->Unlink(target_path);
                       }));
+  environment.Define(
+      Identifier(L"Glob"),
+      vm::NewCallback(
+          pool, PurityType::kReader,
+          [file_system_driver](LazyString pattern)
+              -> futures::ValueOrError<
+                  NonNull<std::shared_ptr<std::vector<std::wstring>>>> {
+            return file_system_driver->Glob(pattern).Transform(
+                [](std::vector<Path> input)
+                    -> language::ValueOrError<
+                        NonNull<std::shared_ptr<std::vector<std::wstring>>>> {
+                  return language::Success(
+                      MakeNonNullShared<std::vector<std::wstring>>(
+                          language::container::MaterializeVector(
+                              input | std::views::transform([](Path& path) {
+                                return path.read();
+                              }))));
+                });
+          }));
 }
 }  // namespace afc::vm
