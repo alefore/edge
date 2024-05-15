9ef38c876ce3e71abf2789fbc4a1c2aca4d0a6fc
Alejandro Forero Cuervo <alefore@gmail.com>
2024-05-15 10:48:05 +0200

    vm: Detect and throw runtime error on empty optional access.
    Previously this was just crashing the vm, which is not optimal.

--- src/vm/optional.h
+++ src/vm/optional.h
@@ -6,7 +6,9 @@
 #include <memory>
 #include <type_traits>
 
+#include "src/language/error/value_or_error.h"
 #include "src/language/gc.h"
+#include "src/language/lazy_string/lazy_string.h"
 #include "src/language/safe_types.h"
 #include "src/vm/value.h"
 #include "src/vm/vm.h"
@@ -54,9 +56,14 @@ void Export(language::gc::Pool& pool, Environment& environment) {
       }).ptr());
   object_type.ptr()->AddField(
       Identifier(L"value"),
-      vm::NewCallback(pool, PurityType::kPure, [](FullType v) {
-        return v->value();
-      }).ptr());
+      vm::NewCallback(pool, PurityType::kPure,
+                      [](FullType v) -> language::ValueOrError<T> {
+                        if (v->has_value()) return v->value();
+                        return language::NewError(
+                            language::lazy_string::LazyString{
+                                L"Optional value has no value."});
+                      })
+          .ptr());
   object_type.ptr()->AddField(
       Identifier(L"reset"),
       vm::NewCallback(pool, PurityType::kUnknown, [](FullType v) {
