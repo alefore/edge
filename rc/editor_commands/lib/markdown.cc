4260033b0a851c2b08a3e586ebae93919567cc0f
Alejandro Forero Cuervo <alefore@gmail.com>
2024-05-15 14:52:17 +0200

    md::FindSection: Implement.

--- rc/editor_commands/lib/markdown.cc
+++ rc/editor_commands/lib/markdown.cc
@@ -1,3 +1,25 @@
+#include "strings.cc"
+
+namespace md {
+namespace internal {
+string GetLineTitle(number depth, string input) {
+  string prefix = "#" * depth + " ";
+  if (!input.starts_with(prefix)) return "";
+  return SkipSpaces(input.substr(prefix.size(), input.size() - prefix.size()));
+}
+
+bool IsLineTitle(string title_expected, number depth, string input) {
+  return GetLineTitle(depth, input) == title_expected;
+}
+
+LineColumn FindSectionEnd(Buffer buffer, number line, number depth) {
+  while (line < buffer.line_count() &&
+         GetLineTitle(depth, buffer.line(line)) == "")
+    line++;
+  return LineColumn(line, 0);
+}
+}  // namespace internal
+
 void Pandoc(string launch_browser) {
   editor.ForEachActiveBuffer([](Buffer buffer) -> void {
     buffer.SetStatus("pandoc ...");
@@ -15,3 +37,13 @@ void Pandoc(string launch_browser) {
     editor.ForkCommand(options);
   });
 }
+
+Range FindSection(Buffer buffer, string title, number depth) {
+  for (number line = 0; line < buffer.line_count(); line++) {
+    if (internal::IsLineTitle(title, depth, buffer.line(line)))
+      return Range(LineColumn(line, 0),
+                   internal::FindSectionEnd(buffer, line + 1, depth));
+  }
+  return Range(LineColumn(0, 0), LineColumn(0, 0));
+}
+}  // namespace md
