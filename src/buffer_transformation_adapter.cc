1554802fca0d83f82b37f0b5ac64a943456da217
Alejandro Forero Cuervo <alefore@gmail.com>
2024-09-10 10:30:22 +0200

    Fragments: start persisting fragments in ~/.edge/fragments.

--- src/buffer_transformation_adapter.cc
+++ src/buffer_transformation_adapter.cc
@@ -2,7 +2,9 @@
 
 #include "src/buffer.h"
 #include "src/buffer_registry.h"
+#include "src/command_argument_mode.h"
 #include "src/editor.h"
+#include "src/file_link_mode.h"
 #include "src/language/error/value_or_error.h"
 #include "src/language/lazy_string/lazy_string.h"
 #include "src/language/text/line.h"
@@ -11,8 +13,11 @@
 
 namespace gc = afc::language::gc;
 
+using afc::infrastructure::Path;
+using afc::infrastructure::PathComponent;
 using afc::language::EmptyValue;
 using afc::language::Error;
+using afc::language::VisitOptional;
 using afc::language::lazy_string::LazyString;
 using afc::language::text::Line;
 using afc::language::text::LineSequence;
@@ -40,15 +45,45 @@ void TransformationInputAdapterImpl::AddError(Error error) {
   buffer_.status().SetInformationText(Line(error.read()));
 }
 
+namespace {
+futures::Value<gc::Root<OpenBuffer>> GetFragmentsBuffer(EditorState& editor) {
+  return VisitOptional(
+      [](gc::Root<OpenBuffer> output) { return futures::Past(output); },
+      [&editor] {
+        return OpenOrCreateFile(
+                   {.editor_state = editor,
+                    .name = FragmentsBuffer{},
+                    .path = editor.edge_path().empty()
+                                ? std::nullopt
+                                : std::make_optional(Path::Join(
+                                      editor.edge_path().front(),
+                                      PathComponent::FromString(L"fragments"))),
+                    .insertion_type = BuffersList::AddBufferType::kIgnore})
+            .Transform([&editor](gc::Root<OpenBuffer> buffer_root) {
+              OpenBuffer& buffer = buffer_root.ptr().value();
+              buffer.Set(buffer_variables::save_on_close, true);
+              buffer.Set(buffer_variables::trigger_reload_on_buffer_write,
+                         false);
+              buffer.Set(buffer_variables::show_in_buffers_list, false);
+              buffer.Set(buffer_variables::atomic_lines, true);
+              buffer.Set(buffer_variables::close_after_idle_seconds, 20.0);
+              buffer.Set(buffer_variables::vm_lines_evaluation, false);
+              if (!editor.has_current_buffer()) {
+                // Seems lame, but what can we do?
+                editor.set_current_buffer(buffer_root,
+                                          CommandArgumentModeApplyMode::kFinal);
+              }
+              return buffer_root.ptr()->WaitForEndOfFile().Transform(
+                  [buffer_root](EmptyValue) { return buffer_root; });
+            });
+      },
+      editor.buffer_registry().Find(FragmentsBuffer{}));
+}
+}  // namespace
+
 void TransformationInputAdapterImpl::AddFragment(LineSequence fragment) {
-  gc::Root<OpenBuffer> fragments_buffer =
-      buffer_.editor().buffer_registry().MaybeAdd(
-          FragmentsBuffer{}, [&editor = buffer_.editor()] {
-            return OpenBuffer::New(
-                {.editor = editor, .name = FragmentsBuffer{}});
-          });
-  fragments_buffer.ptr()->WaitForEndOfFile().Transform(
-      [fragments_buffer, fragment](EmptyValue) {
+  GetFragmentsBuffer(buffer_.editor())
+      .Transform([fragment](gc::Root<OpenBuffer> fragments_buffer) {
         fragments_buffer.ptr()->AppendLine(vm::EscapedMap{
             std::multimap<vm::Identifier, LazyString>{
                 {vm::Identifier{LazyString{L"fragment"}},
