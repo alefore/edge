9265f33668d9f5b94552dc79b9b84f703df4e6fb
Alejandro Forero Cuervo <alefore@gmail.com>
2024-09-10 16:47:30 +0200

    Remove unnecessary conversion to std::wstring.

--- src/buffer_vm.cc
+++ src/buffer_vm.cc
@@ -415,7 +415,7 @@ void DefineBufferType(gc::Pool& pool, Environment& environment) {
                             std::min(LineNumber(std::max(line_input, 0)),
                                      LineNumber(0) + buffer->lines_size() -
                                          LineNumberDelta(1));
-                        return buffer->contents().at(line).ToString();
+                        return buffer->contents().at(line).contents();
                       })
           .ptr());
 
