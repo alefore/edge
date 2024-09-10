038776af0b330c560340adc226bb810e15a05322
Alejandro Forero Cuervo <alefore@gmail.com>
2024-09-10 20:44:33 +0200

    SingleLine: Define.

---
+++ src/language/lazy_string/single_line.cc
@@ -0,0 +1,13 @@
+#include "src/language/lazy_string/single_line.h"
+
+#include "src/language/error/value_or_error.h"
+#include "src/language/lazy_string/functional.h"
+
+namespace afc::language::lazy_string {
+/* static */ language::PossibleError SingleLineValidator::Validate(
+    const LazyString& input) {
+  if (FindFirstOf(input, {L'\n'}).has_value())
+    return Error{LazyString{L"SingleLine contained newline character."}};
+  return EmptyValue{};
+}
+}  // namespace afc::language::lazy_string
---
+++ src/language/lazy_string/single_line.h
@@ -0,0 +1,17 @@
+#ifndef __AFC_LANGUAGE_LAZY_STRING_SINGLE_LINE_H__
+#define __AFC_LANGUAGE_LAZY_STRING_SINGLE_LINE_H__
+
+#include "src/language/error/value_or_error.h"
+#include "src/language/ghost_type_class.h"
+#include "src/language/lazy_string/lazy_string.h"
+
+namespace afc::language::lazy_string {
+struct SingleLineValidator {
+  static language::PossibleError Validate(const LazyString& input);
+};
+
+class SingleLine
+    : public GhostType<SingleLine, LazyString, SingleLineValidator> {};
+}  // namespace afc::language::lazy_string
+
+#endif
