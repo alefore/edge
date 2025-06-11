d5996b8e086c481f80921a50475b8968637fea86
Alejandro Forero Cuervo <alefore@gmail.com>
2025-06-11 16:15:17 +0200

    Fix bug in HumanReadableDate.

--- src/infrastructure/time_human.cc
+++ src/infrastructure/time_human.cc
@@ -11,25 +11,35 @@ using afc::language::lazy_string::SingleLine;
 
 namespace afc::infrastructure {
 namespace {
-language::ValueOrError<NonEmptySingleLine> strftime(const struct timespec& time,
-                                                    const char* spec) {
+enum class NanosecondsBehavior { kIgnore, kAppend };
+language::ValueOrError<NonEmptySingleLine> strftime(
+    const struct timespec& time, const char* spec,
+    NanosecondsBehavior nanoseconds_behavior) {
   struct tm tm_value;
   if (localtime_r(&time.tv_sec, &tm_value) == nullptr)
     return Error{LazyString{L"localtime_r failed"}};
   char buffer[1024];
   size_t len = strftime(buffer, sizeof(buffer), spec, &tm_value);
   if (len == 0) return Error{LazyString{L"strftime failed"}};
-  snprintf(buffer + len, sizeof(buffer) - len, ".%09ld", time.tv_nsec);
+  switch (nanoseconds_behavior) {
+    case NanosecondsBehavior::kAppend:
+      snprintf(buffer + len, sizeof(buffer) - len, ".%09ld", time.tv_nsec);
+      break;
+    case NanosecondsBehavior::kIgnore:
+      break;
+    default:
+      LOG(FATAL) << "Invalid value for nanoseconds_behavior.";
+  }
   return NonEmptySingleLine::New(SingleLine::New(LazyString{
       language::FromByteString(std::string(buffer, strlen(buffer)))}));
 }
 }  // namespace
 
 language::ValueOrError<NonEmptySingleLine> HumanReadableTime(const Time& time) {
-  return strftime(time, "%Y-%m-%d %T %z");
+  return strftime(time, "%Y-%m-%d %T %z", NanosecondsBehavior::kAppend);
 }
 
 language::ValueOrError<NonEmptySingleLine> HumanReadableDate(const Time& time) {
-  return strftime(time, "%Y-%m-%d");
+  return strftime(time, "%Y-%m-%d", NanosecondsBehavior::kIgnore);
 }
 }  // namespace afc::infrastructure
