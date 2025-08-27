29c6e13f299d7372ae8c5be53959f9b4a838d74f
Alejandro Forero Cuervo <alefore@gmail.com>
2025-08-27 17:18:07 +0200

    ParseQuotedString tests: Various improvements (no op).

--- src/parsers/util_test.cc
+++ src/parsers/util_test.cc
@@ -28,7 +28,7 @@ using afc::language::text::LineNumber;
 using afc::language::text::LineSequence;
 using afc::language::text::Range;
 
-namespace afc::editor {
+namespace afc::editor::parsers {
 
 using ::operator<<;
 
@@ -36,25 +36,26 @@ tests::Test TestParseQuotedString(
     const std::wstring& name, const std::wstring& input_string,
     wchar_t quote_char, const LineModifierSet& kContentModifiers,
     const std::unordered_set<ParseTreeProperty>& kContentProperties,
-    std::optional<parsers::NestedExpressionSyntax> nested_expression_syntax,
-    ParseTree expected_tree) {
+    std::optional<NestedExpressionSyntax> nested_expression_syntax,
+    MultipleLinesSupport multiple_lines_support,
+    ParseQuotedStringState expected_state, ParseTree expected_tree) {
   return tests::Test{
       .name = name, .callback = [=] {
         LineSequence contents = LineSequence::ForTests({input_string});
         ParseData parse_data(contents, {}, LineColumn{LineNumber{1}});
 
-        std::vector<ParseTree> parse_trees = {ParseTree(Range(
-            LineColumn{LineNumber{0}, ColumnNumber{0}},
-            LineColumn{LineNumber{0}, ColumnNumber{input_string.size()}}))};
-
         // Skip opening quote, as expected by ParseQuotedString.
         parse_data.seek().Once();
 
-        parsers::ParseQuotedStringState state = parsers::ParseQuotedString(
-            &parse_data, quote_char, kContentModifiers, kContentProperties,
-            nested_expression_syntax, parsers::MultipleLinesSupport::kReject,
-            parsers::CurrentState::kStart);
-        CHECK_EQ(state, parsers::ParseQuotedStringState::kDone);
+        CHECK_EQ(
+            ParseQuotedString(&parse_data, quote_char, kContentModifiers,
+                              kContentProperties, nested_expression_syntax,
+                              multiple_lines_support, CurrentState::kStart),
+            expected_state);
+
+        std::vector<ParseTree> parse_trees = {ParseTree(Range(
+            LineColumn{LineNumber{0}, ColumnNumber{0}},
+            LineColumn{LineNumber{0}, ColumnNumber{input_string.size()}}))};
 
         for (const auto& action : parse_data.parse_results().actions)
           Execute(action, &parse_trees, LineNumber{0});
@@ -84,7 +85,7 @@ static const NonEmptySingleLine kNestedSyntaxSuffix =
 static const LineModifierSet kContentModifiers = {LineModifier::kBold};
 static const std::unordered_set<ParseTreeProperty> kContentProperties = {
     ParseTreeProperty::StringValue()};
-LineModifierSet nested_prefix_suffix_modifiers = {LineModifier::kDim};
+LineModifierSet kNestedPrefixSuffixModifiers = {LineModifier::kDim};
 LineModifierSet kNestedContentModifiers = {LineModifier::kGreen};
 
 bool parse_quoted_string_tests = afc::tests::Register(
@@ -92,7 +93,8 @@ bool parse_quoted_string_tests = afc::tests::Register(
     std::vector<afc::tests::Test>{
         TestParseQuotedString(
             L"EmptyString", L"\"\"", L'\"', kContentModifiers,
-            kContentProperties, std::nullopt,
+            kContentProperties, std::nullopt, MultipleLinesSupport::kReject,
+            ParseQuotedStringState::kDone,
             NewTree(ColumnNumber{0}, SINGLE_LINE_CONSTANT(L"\"\""), {}, {},
                     {NewTree(ColumnNumber{0}, kOpeningQuote,
                              LineModifierSet{LineModifier::kDim}, {}, {}),
@@ -102,7 +104,8 @@ bool parse_quoted_string_tests = afc::tests::Register(
                              LineModifierSet{LineModifier::kDim}, {}, {})})),
         TestParseQuotedString(
             L"SimpleString", L"\"hello\"", L'\"', kContentModifiers,
-            kContentProperties, std::nullopt,
+            kContentProperties, std::nullopt, MultipleLinesSupport::kReject,
+            ParseQuotedStringState::kDone,
             NewTree(
                 ColumnNumber{0}, SINGLE_LINE_CONSTANT(L"\"hello\""), {}, {},
                 {NewTree(ColumnNumber{0}, kOpeningQuote,
@@ -116,11 +119,12 @@ bool parse_quoted_string_tests = afc::tests::Register(
         TestParseQuotedString(
             L"NestedExpressionString", L"\"foo {expr} bar\"", L'\"',
             kContentModifiers, kContentProperties,
-            parsers::NestedExpressionSyntax{
+            NestedExpressionSyntax{
                 .prefix = kNestedSyntaxPrefix,
                 .suffix = kNestedSyntaxSuffix,
-                .prefix_suffix_modifiers = nested_prefix_suffix_modifiers,
+                .prefix_suffix_modifiers = kNestedPrefixSuffixModifiers,
                 .modifiers = kNestedContentModifiers},
+            MultipleLinesSupport::kReject, ParseQuotedStringState::kDone,
             NewTree(
                 ColumnNumber{0},
                 SINGLE_LINE_CONSTANT(L"\"foo {expr} bar\""), {}, {},
@@ -133,11 +137,11 @@ bool parse_quoted_string_tests = afc::tests::Register(
                          NewTree(ColumnNumber{1}, SINGLE_LINE_CONSTANT(L"foo "),
                                  kContentModifiers, kContentProperties, {}),
                          NewTree(ColumnNumber{5}, kNestedSyntaxPrefix.read(),
-                                 nested_prefix_suffix_modifiers, {}, {}),
+                                 kNestedPrefixSuffixModifiers, {}, {}),
                          NewTree(ColumnNumber{6}, SINGLE_LINE_CONSTANT(L"expr"),
                                  kNestedContentModifiers, {}, {}),
                          NewTree(ColumnNumber{10}, kNestedSyntaxSuffix.read(),
-                                 nested_prefix_suffix_modifiers, {}, {}),
+                                 kNestedPrefixSuffixModifiers, {}, {}),
                          NewTree(ColumnNumber{11},
                                  SINGLE_LINE_CONSTANT(L" bar"),
                                  kContentModifiers, kContentProperties, {}),
@@ -148,11 +152,12 @@ bool parse_quoted_string_tests = afc::tests::Register(
             L"MultipleNestedExpressions",
             L"\"foo {i + 1} bar quux {some.call()} nah\"", L'\"',
             kContentModifiers, kContentProperties,
-            parsers::NestedExpressionSyntax{
-                .prefix = NON_EMPTY_SINGLE_LINE_CONSTANT(L"{"),
-                .suffix = NON_EMPTY_SINGLE_LINE_CONSTANT(L"}"),
-                .prefix_suffix_modifiers = nested_prefix_suffix_modifiers,
+            NestedExpressionSyntax{
+                .prefix = kNestedSyntaxPrefix,
+                .suffix = kNestedSyntaxSuffix,
+                .prefix_suffix_modifiers = kNestedPrefixSuffixModifiers,
                 .modifiers = kNestedContentModifiers},
+            MultipleLinesSupport::kReject, ParseQuotedStringState::kDone,
             NewTree(ColumnNumber{0},
                     SINGLE_LINE_CONSTANT(
                         L"\"foo {i + 1} bar quux {some.call()} nah\""),
@@ -171,25 +176,25 @@ bool parse_quoted_string_tests = afc::tests::Register(
                                  kContentModifiers, kContentProperties, {}),
                              NewTree(ColumnNumber{5},
                                      kNestedSyntaxPrefix.read(),
-                                     nested_prefix_suffix_modifiers, {}, {}),
+                                     kNestedPrefixSuffixModifiers, {}, {}),
                              NewTree(ColumnNumber{6},
                                      SINGLE_LINE_CONSTANT(L"i + 1"),
                                      kNestedContentModifiers, {}, {}),
                              NewTree(ColumnNumber{11},
                                      kNestedSyntaxSuffix.read(),
-                                     nested_prefix_suffix_modifiers, {}, {}),
+                                     kNestedPrefixSuffixModifiers, {}, {}),
                              NewTree(ColumnNumber{12},
                                      SINGLE_LINE_CONSTANT(L" bar quux "),
                                      kContentModifiers, kContentProperties, {}),
                              NewTree(ColumnNumber{22},
                                      kNestedSyntaxPrefix.read(),
-                                     nested_prefix_suffix_modifiers, {}, {}),
+                                     kNestedPrefixSuffixModifiers, {}, {}),
                              NewTree(ColumnNumber{23},
                                      SINGLE_LINE_CONSTANT(L"some.call()"),
                                      kNestedContentModifiers, {}, {}),
                              NewTree(ColumnNumber{34},
                                      kNestedSyntaxSuffix.read(),
-                                     nested_prefix_suffix_modifiers, {}, {}),
+                                     kNestedPrefixSuffixModifiers, {}, {}),
                              NewTree(ColumnNumber{35},
                                      SINGLE_LINE_CONSTANT(L" nah"),
                                      kContentModifiers, kContentProperties, {}),
@@ -197,4 +202,4 @@ bool parse_quoted_string_tests = afc::tests::Register(
                      NewTree(ColumnNumber{39}, kClosingQuote,
                              LineModifierSet{LineModifier::kDim}, {}, {})}))});
 
-}  // namespace afc::editor
+}  // namespace afc::editor::parsers
