8797812c1d8d35236c8e78a197ee3b0805be8075
Alejandro Forero Cuervo <alefore@gmail.com>
2025-09-22 01:01:50 +0200

    Improve CSS to support one specific property.
    I went with border-style as a simple rule, just to see how this would work.

--- src/parsers/css.cc
+++ src/parsers/css.cc
@@ -46,10 +46,30 @@ enum State {
   IN_MULTI_LINE_COMMENT,
   IN_MULTIPLE_LINE_STRING_SINGLE_QUOTE,
   IN_MULTIPLE_LINE_STRING_DOUBLE_QUOTE,
+  IN_DECLARATION_BLOCK_EXPECT_PROPERTY,
+  IN_DECLARATION_BLOCK_EXPECT_COLON,
+  IN_DECLARATION_BLOCK_EXPECT_VALUE,
+  PROPERTY_BORDER_STYLE_COLON,
+  PROPERTY_BORDER_STYLE_VALUE,
+  INVALID_PROPERTY_VALUE,
 };
 
 static const LineModifierSet BAD_PARSE_MODIFIERS =
     LineModifierSet({LineModifier::kBgRed, LineModifier::kBold});
+
+static const LineModifierSet RECOGNIZED_RULE_MODIFIERS =
+    LineModifierSet({LineModifier::kMagenta, LineModifier::kBold});
+
+static const LineModifierSet VALID_BORDER_STYLE_VALUE_MODIFIERS =
+    LineModifierSet({LineModifier::kGreen});
+
+static const std::unordered_set<SingleLine> kBorderStyleValues =
+    container::MaterializeUnorderedSet(std::vector<SingleLine>{
+        SINGLE_LINE_CONSTANT(L"none"), SINGLE_LINE_CONSTANT(L"hidden"),
+        SINGLE_LINE_CONSTANT(L"dotted"), SINGLE_LINE_CONSTANT(L"dashed"),
+        SINGLE_LINE_CONSTANT(L"solid"), SINGLE_LINE_CONSTANT(L"double"),
+        SINGLE_LINE_CONSTANT(L"groove"), SINGLE_LINE_CONSTANT(L"ridge"),
+        SINGLE_LINE_CONSTANT(L"inset"), SINGLE_LINE_CONSTANT(L"outset")});
 
 enum class HashToModifiersBold { kSometimes, kNever };
 LineModifierSet HashToModifiers(int nesting,
@@ -80,10 +100,19 @@ class CssTreeParser : public parsers::LineOrientedTreeParser {
     while (!done) {
       LineColumn original_position = result->position();
       done = result->seek().read() == L'\n';
+      VLOG(5) << "State: " << result->state();
 
       switch (result->state()) {
         case DEFAULT:
+        case IN_DECLARATION_BLOCK_EXPECT_PROPERTY:
+        case IN_DECLARATION_BLOCK_EXPECT_VALUE:
+        case PROPERTY_BORDER_STYLE_VALUE:
+        case INVALID_PROPERTY_VALUE:
           ParseDefaultState(result);
+          break;
+        case IN_DECLARATION_BLOCK_EXPECT_COLON:
+        case PROPERTY_BORDER_STYLE_COLON:
+          ParseColon(result);
           break;
         case IN_MULTI_LINE_COMMENT:
           ParseMultiLineComment(result);
@@ -98,6 +127,34 @@ class CssTreeParser : public parsers::LineOrientedTreeParser {
   }
 
  private:
+  void ParseColon(ParseData* result) {
+    Seek seek = result->seek();
+    wchar_t c = seek.read();
+
+    if (c == L'\n') return;
+
+    seek.Once();
+
+    if (c == L'\t' || c == L' ') return;
+    if (c != L':') {
+      result->PushAndPop(ColumnNumberDelta(1), BAD_PARSE_MODIFIERS);
+      if (c == L';') result->SetState(IN_DECLARATION_BLOCK_EXPECT_PROPERTY);
+      return;
+    }
+
+    result->PushAndPop(ColumnNumberDelta(1), {LineModifier::kDim});
+    switch (result->state()) {
+      case PROPERTY_BORDER_STYLE_COLON:
+        result->SetState(PROPERTY_BORDER_STYLE_VALUE);
+        break;
+      case IN_DECLARATION_BLOCK_EXPECT_COLON:
+        result->SetState(IN_DECLARATION_BLOCK_EXPECT_VALUE);
+        break;
+      default:
+        LOG(FATAL) << "ParseColon called in invalid state.";
+    }
+  }
+
   // Invariant: the previous character (before result->position()) must be in
   // `char_set`.
   void ParseWordLikeToken(ParseData* result,
@@ -147,7 +204,8 @@ class CssTreeParser : public parsers::LineOrientedTreeParser {
     }
 
     if (c == L'{') {
-      result->Push(DEFAULT, ColumnNumberDelta(1), {}, {});
+      result->Push(IN_DECLARATION_BLOCK_EXPECT_PROPERTY, ColumnNumberDelta(1),
+                   {}, {});
       result->PushAndPop(ColumnNumberDelta(1), BAD_PARSE_MODIFIERS);
       return;
     }
@@ -159,41 +217,93 @@ class CssTreeParser : public parsers::LineOrientedTreeParser {
         result->PushAndPop(ColumnNumberDelta(1), modifiers);
         result->SetFirstChildModifiers(modifiers);
         result->PopBack();
+        // After popping, if the previous state was a declaration block, reset
+        // to DEFAULT as we're outside a declaration.
+        if (result->state() == IN_DECLARATION_BLOCK_EXPECT_PROPERTY ||
+            result->state() == IN_DECLARATION_BLOCK_EXPECT_VALUE ||
+            result->state() == PROPERTY_BORDER_STYLE_VALUE ||
+            result->state() == INVALID_PROPERTY_VALUE) {
+          result->SetState(DEFAULT);
+        }
       } else
         result->PushAndPop(ColumnNumberDelta(1), BAD_PARSE_MODIFIERS);
       return;
     }
 
-    if (result->parse_results().states_stack.size() > 1) {
-      if (c == L':')
-        result->PushAndPop(ColumnNumberDelta(1), {LineModifier::kDim});
-      else if (c == L';')
-        result->PushAndPop(ColumnNumberDelta(1), {LineModifier::kDim});
-      else {
-        static const std::unordered_set<wchar_t> css_prop_val_chars =
+    switch (result->state()) {
+      case DEFAULT: {
+        static const std::unordered_set<wchar_t> css_selector_chars =
             container::MaterializeUnorderedSet(
                 std::wstring_view{L"_-"
                                   L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq"
                                   L"rstuvwxyz0123456789"
                                   L"."
-                                  L"%"});
-        if (css_prop_val_chars.contains(c)) {
-          ParseWordLikeToken(result, css_prop_val_chars,
-                             {LineModifier::kWhite});
+                                  L"#"
+                                  L"*"});
+        if (css_selector_chars.contains(c)) {
+          ParseWordLikeToken(result, css_selector_chars, {LineModifier::kCyan});
           return;
         }
+        break;
       }
-    } else {
-      static const std::unordered_set<wchar_t> css_selector_chars =
-          container::MaterializeUnorderedSet(
-              std::wstring_view{L"_-"
-                                L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq"
-                                L"rstuvwxyz0123456789"
-                                L"."
-                                L"#"
-                                L"*"});
-      if (css_selector_chars.contains(c)) {
-        ParseWordLikeToken(result, css_selector_chars, {LineModifier::kCyan});
+      case IN_DECLARATION_BLOCK_EXPECT_PROPERTY:
+      case IN_DECLARATION_BLOCK_EXPECT_VALUE:
+      case PROPERTY_BORDER_STYLE_VALUE:
+      case INVALID_PROPERTY_VALUE: {
+        if (c == L':') {
+          // Missing the property.
+          result->PushAndPop(ColumnNumberDelta(1), BAD_PARSE_MODIFIERS);
+        } else if (c == L';') {
+          result->PushAndPop(ColumnNumberDelta(1), {LineModifier::kDim});
+          // After a semicolon, we expect a new property.
+          result->SetState(IN_DECLARATION_BLOCK_EXPECT_PROPERTY);
+        } else {
+          static const std::unordered_set<wchar_t> css_prop_val_chars =
+              container::MaterializeUnorderedSet(std::wstring_view{
+                  L"_-"
+                  L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq"
+                  L"rstuvwxyz0123456789"
+                  L"."
+                  L"%"});
+          if (css_prop_val_chars.contains(c)) {
+            LineColumn original_word_start =
+                result->position() - ColumnNumberDelta{1};
+            result->seek().UntilCurrentCharNotIn(css_prop_val_chars);
+            ColumnNumberDelta length =
+                result->position().column - original_word_start.column;
+            CHECK_GT(length, ColumnNumberDelta{});
+
+            const auto& line_object =
+                result->buffer().at(result->position().line);
+            const SingleLine& current_line_contents = line_object.contents();
+            SingleLine token = current_line_contents.Substring(
+                original_word_start.column, length);
+
+            if (result->state() == IN_DECLARATION_BLOCK_EXPECT_PROPERTY) {
+              if (token == SINGLE_LINE_CONSTANT(L"border-style")) {
+                result->PushAndPop(length, RECOGNIZED_RULE_MODIFIERS);
+                result->SetState(PROPERTY_BORDER_STYLE_COLON);
+              } else {
+                result->PushAndPop(length, {LineModifier::kWhite});
+                result->SetState(IN_DECLARATION_BLOCK_EXPECT_COLON);
+              }
+            } else if (result->state() == PROPERTY_BORDER_STYLE_VALUE) {
+              VLOG(5) << "Check value: " << token;
+              if (kBorderStyleValues.contains(token)) {
+                result->PushAndPop(length, VALID_BORDER_STYLE_VALUE_MODIFIERS);
+                // TODO(trivial, 2025-09-22):
+                // result->SetState(IN_DECLARATION_BLOCK_EXPECT_SEMICOLON);
+              } else {
+                result->PushAndPop(length, BAD_PARSE_MODIFIERS);
+                result->SetState(INVALID_PROPERTY_VALUE);
+              }
+            } else if (result->state() == INVALID_PROPERTY_VALUE) {
+              result->PushAndPop(length, BAD_PARSE_MODIFIERS);
+            } else {  // IN_DECLARATION_BLOCK_EXPECT_VALUE
+              result->PushAndPop(length, {LineModifier::kWhite});
+            }
+          }
+        }
         return;
       }
     }
@@ -240,7 +350,7 @@ class CssTreeParser : public parsers::LineOrientedTreeParser {
         ParseQuotedStringState::kDone)
       return;
     result->SetState(original_state);
-    CHECK(result->seek().read() == '\n');
+    CHECK(result->seek().read() == L'\n');
   }
 };
 }  // namespace
