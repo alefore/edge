c1a2633a3b64464d3c97c756798cd87240731856
Alejandro Forero Cuervo <alefore@gmail.com>
2026-04-30 10:43:47 +0200

    Augment format for log configs.

--- src/log_config_loader.cc
+++ src/log_config_loader.cc
@@ -8,17 +8,32 @@
 #include <unordered_map>
 #include <vector>
 
+#include "src/futures/futures.h"
+#include "src/infrastructure/dirname.h"
 #include "src/language/error/view.h"
+#include "src/language/gc.h"
 #include "src/language/lazy_string/convert.h"
+#include "src/language/lazy_string/lowercase.h"
 #include "src/language/lazy_string/trim.h"
 #include "src/language/text/line.h"
+#include "src/open_files.h"
 
+namespace container = afc::language::container;
+namespace gc = afc::language::gc;
+
+using afc::futures::UnwrapVectorFuture;
+using afc::infrastructure::Path;
+using afc::language::AugmentError;
+using afc::language::EmptyValue;
 using afc::language::Error;
+using afc::language::PossibleError;
+using afc::language::ValueOrError;
 using afc::language::lazy_string::ColumnNumber;
 using afc::language::lazy_string::ColumnNumberDelta;
 using afc::language::lazy_string::LazyString;
 using afc::language::lazy_string::NonEmptySingleLine;
 using afc::language::lazy_string::SingleLine;
+using afc::language::lazy_string::ToLazyString;
 using afc::language::lazy_string::Trim;
 using afc::language::text::Line;
 using afc::language::text::LineColumn;
@@ -26,6 +41,7 @@ using afc::language::text::LineNumber;
 using afc::language::text::LineNumberDelta;
 using afc::language::text::LineSequence;
 using afc::language::text::Range;
+using afc::language::view::GetErrors;
 using afc::language::view::SkipErrors;
 
 namespace afc::editor {
@@ -37,25 +53,29 @@ auto GetBlockIndices(LineSequence lines) {
                   EndsWith(line.contents(), LazyString{L"]"});
          }) |
          std::views::transform([](std::tuple<size_t, const Line&> input) {
-           return LineNumber{std::get<0>(input)};
+           return LineColumn{LineNumber{std::get<0>(input)}};
          });
 }
 
-auto PartitionIntoBlocks(LineSequence lines) {
+std::vector<LineSequence> PartitionIntoBlocks(LineSequence lines) {
   // TODO(P2, C++26, 2026-04-28): Avoid the conversion to vector once we can
   // use std::views::concat:
   //    std::views::concat(GetBlockIndices(lines),
   //                       std::views::single(LineNumber{} + lines.size())
-  std::vector<LineNumber> indices =
+  std::vector<LineColumn> indices =
       GetBlockIndices(lines) | std::ranges::to<std::vector>();
-  indices.push_back(LineNumber{} + lines.size());
+  indices.push_back(lines.range().end());
   return indices | std::views::adjacent<2> |
          std::views::transform(
-             [lines](std::tuple<LineNumber, LineNumber> entry) {
-               return lines.ViewRange(Range{LineColumn{std::get<0>(entry)},
-                                            LineColumn{std::get<1>(entry)}});
-             });
+             [lines](std::tuple<LineColumn, LineColumn> entry) {
+               Range range{std::get<0>(entry), std::get<1>(entry)};
+               CHECK_LT(range.begin().line, range.end().line);
+               return lines.ViewRange(range);
+             }) |
+         std::ranges::to<std::vector>();
 }
+
+namespace {}
 
 std::expected<LogType, Error> ParseLogType(const LineSequence& block) {
   if (block.size() < LineNumberDelta{2}) return Error{L"Short block found."};
@@ -75,32 +95,61 @@ std::expected<LogType, Error> ParseLogType(const LineSequence& block) {
                              {L' '})));
   LogTypeName log_type_name{log_type_name_str};
 
-  SingleLine pattern = Trim(block.at(LineNumber{1}).contents(), {L' '});
-  if (pattern.empty()) return Error{L"Empty pattern found."};
-
+  std::optional<NonEmptySingleLine> pattern;
   std::unordered_map<LogEntryName, LogEntryConfiguration> entries;
-
-  for (LineNumber i{2}; i.ToDelta() < block.size(); ++i) {
-    SingleLine line_str = Trim(block.at(i).contents(), {L' '});
-    if (line_str.empty() || StartsWith(line_str, LazyString{L"#"})) continue;
-
-    std::optional<ColumnNumber> first_space = FindFirstOf(line_str, {L' '});
-    if (!first_space.has_value())
-      return Error{LazyString{L"Invalid line detected (missing space): "} +
-                   line_str};
-
-    DECLARE_OR_RETURN(
-        int group_id,
-        AsInt(ToLazyString(Trim(
-            line_str.Substring(0, first_space.value().ToDelta()), {L' '}))));
-
-    LogEntryName name{
-        Trim(line_str.Substring(first_space.value() + ColumnNumberDelta{1}),
-             {L' '})};
-
-    entries[name] = LogEntryConfiguration{LogCapturingGroup{group_id}};
-  }
-  return LogType(log_type_name, pattern, std::move(entries));
+  static const std::unordered_set<wchar_t> Space = {L' '};
+  std::vector<Error> errors =
+      block | std::views::drop(1) |
+      std::views::transform([&](Line line) -> PossibleError {
+        SingleLine line_str = TrimLeft(line.contents(), Space);
+        if (line_str.empty() || StartsWith(line_str, LazyString{L"#"}))
+          return EmptyValue{};
+        std::optional<ColumnNumber> first_colon = FindFirstOf(line_str, {L':'});
+        if (!first_colon.has_value())
+          return Error{LazyString{L"Invalid line detected (missing colon): "} +
+                       line_str};
+        SingleLine directive =
+            LowerCase(line_str.Substring(first_colon.value()));
+        SingleLine value =
+            Trim(line_str.Substring(first_colon.value() + ColumnNumberDelta{1}),
+                 Space);
+        if (value.empty()) return Error{L"Value missing."};
+        if (directive == NON_EMPTY_SINGLE_LINE_CONSTANT(L"pattern")) {
+          if (pattern.has_value())
+            return Error{L"Pattern specified multiple times."};
+          DECLARE_OR_RETURN(NonEmptySingleLine pattern_value,
+                            AugmentError(L"Producing pattern",
+                                         NonEmptySingleLine::New(value)));
+          pattern = pattern_value;
+          return EmptyValue{};
+        }
+        if (directive == NON_EMPTY_SINGLE_LINE_CONSTANT(L"group")) {
+          std::optional<ColumnNumber> group_id_end = FindFirstOf(value, Space);
+          std::optional<SingleLine> entry_name_str =
+              group_id_end.transform([&](ColumnNumber pos) {
+                return Trim(value.Substring(pos), Space);
+              });
+          if (!entry_name_str) return Error{L"Expected: entry name."};
+          DECLARE_OR_RETURN(NonEmptySingleLine entry_name_non_empty,
+                            NonEmptySingleLine::New(entry_name_str.value()));
+          return Visit(
+              AsInt(ToLazyString(
+                  value.Substring(ColumnNumber{}, group_id_end->ToDelta()))),
+              [&](int group_id) -> PossibleError {
+                entries[LogEntryName{entry_name_non_empty}] =
+                    LogEntryConfiguration{LogCapturingGroup{group_id}};
+                return EmptyValue{};
+              },
+              [&errors](Error error) -> PossibleError {
+                return AugmentError(L"Invalid group ID", error);
+              });
+        }
+        return Error{LazyString{L"Invalid directive: "} + directive};
+      }) |
+      GetErrors | std::ranges::to<std::vector>();
+  if (!pattern) errors.push_back(Error{L"No pattern specified."});
+  if (!errors.empty()) return MergeErrors(errors, L", ");
+  return LogType(log_type_name, pattern.value(), std::move(entries));
 }
 
 std::expected<LogModel, language::Error> ParseLogConfig(
@@ -117,14 +166,61 @@ std::expected<LogModel, language::Error> ParseLogConfig(
                 return std::make_pair(log_type.name(), log_type);
               }) |
           SkipErrors | std::ranges::to<std::unordered_map>()};
+  LOG(INFO) << "Returning model!";
   if (!errors.empty()) return MergeErrors(errors, L", ");
   return model;
 }
 
 futures::ValueOrError<LogModel> LoadModelFromPaths(
-    EditorState&, language::lazy_string::LazyString) {
-  // TODO(P2, log): Implement.
-  return LogModel{};
+    EditorState& editor, language::lazy_string::LazyString paths) {
+  LOG(INFO) << "LoadModelFromPaths: " << paths;
+  // TODO(P1, 2026-04-29, log): Break paths into comma-separated list of
+  // paths.
+  DECLARE_OR_RETURN(Path input_path, Path::New(paths));
+  return UnwrapVectorFuture(
+             editor.edge_path() |
+             std::views::transform([&editor, input_path](Path edge_path)
+                                       -> futures::ValueOrError<LogModel> {
+               DECLARE_OR_RETURN(SingleLine path_pattern,
+                                 SingleLine::New(ToLazyString(
+                                     Path::Join(edge_path, input_path))));
+               return OpenFiles(
+                          OpenFilesOptions{
+                              .editor = editor,
+                              .not_found_handler =
+                                  OpenFilesOptions::NotFoundHandler::kIgnore,
+                              .path_pattern = path_pattern,
+                              .insertion_type =
+                                  BuffersList::AddBufferType::kIgnore,
+                              .directory_filter =
+                                  FilePredictorOptions::Filter::Exclude,
+                              .special_file_filter =
+                                  FilePredictorOptions::Filter::Exclude})
+                   .Transform(
+                       [](std::vector<gc::Root<OpenBuffer>> files)
+                           -> futures::ValueOrError<gc::Root<OpenBuffer>> {
+                         // TODO(P0, 2026-04-29, log): Don't just handle the
+                         // first one! Don't ignore the rest.
+                         if (files.empty()) return Error{L"No models found."};
+                         return files[0]->WaitForEndOfFile();
+                       })
+                   .Transform([](gc::Root<OpenBuffer> model_root)
+                                  -> futures::ValueOrError<LogModel> {
+                     LineSequence snapshot = model_root->contents().snapshot();
+                     LOG(INFO)
+                         << "Parsing log model from " << model_root->name()
+                         << ", lines: " << snapshot.size();
+                     return model_root->editor().thread_pool().Run(
+                         [snapshot] mutable {
+                           return ParseLogConfig(std::move(snapshot));
+                         });
+                   });
+             }) |
+             std::ranges::to<std::vector>())
+      .Transform([](std::vector<ValueOrError<LogModel>> model) {
+        // TODO(P0, 2026-04-29, log): Don't just return the first.
+        return model[0];
+      });
 }
 
 }  // namespace afc::editor
