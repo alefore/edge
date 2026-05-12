566703ac9cbaf428c00bc5eff3514bc16c14e249
Alejandro Cuervo <alefore@gmail.com>
2026-05-12 14:36:06 +0200

    Delete unnecessary (and redundant) definitions.

--- src/language/lazy_string/functional_tests.cc
+++ src/language/lazy_string/functional_tests.cc
@@ -1,5 +1,4 @@
 #include "src/language/lazy_string/functional.h"
-
 #include "src/tests/tests.h"
 
 namespace afc::language::lazy_string {
@@ -68,35 +67,4 @@ const bool find_first_of_tests_registration = tests::Register(
       }}});
 
 }  // namespace
-std::vector<LazyString> SplitAt(LazyString input, wchar_t separator) {
-  std::vector<LazyString> output;
-  std::optional<ColumnNumber> start = ColumnNumber{};
-  while (start.has_value()) {
-    start = VisitOptional(
-        [&](ColumnNumber next) -> std::optional<ColumnNumber> {
-          output.push_back(
-              input.Substring(start.value(), next - start.value()));
-          return next + ColumnNumberDelta{1};
-        },
-        [&]() -> std::optional<ColumnNumber> {
-          output.push_back(input.Substring(start.value()));
-          return std::nullopt;
-        },
-        FindFirstOf(input, {separator}, start.value()));
-  }
-  return output;
-}
 }  // namespace afc::language::lazy_string
-
-namespace std {
-using afc::language::hash_combine;
-using afc::language::MakeHashableIteratorRange;
-
-std::size_t hash<afc::language::lazy_string::LazyString>::operator()(
-    const afc::language::lazy_string::LazyString& input) const {
-  size_t value = 302948;
-  ForEachColumn(input, [&](afc::language::lazy_string::ColumnNumber,
-                           wchar_t c) { value = hash_combine(value, c); });
-  return value;
-}
-}  // namespace std
