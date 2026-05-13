bfab47cebd81510bed0589f82c84ff2b308da996
Alejandro Cuervo <alefore@gmail.com>
2026-05-13 14:22:31 +0200

    Adopt TEST_GROUP for LazyString tests.

--- src/language/lazy_string/functional_tests.cc
+++ src/language/lazy_string/functional_tests.cc
@@ -1,70 +1,50 @@
 #include "src/language/lazy_string/functional.h"
+#include "src/tests/factory.h"
 #include "src/tests/tests.h"
 
 namespace afc::language::lazy_string {
 namespace {
-const bool starts_with_tests_registration = tests::Register(
-    L"LazyString::StartsWith",
-    {
-        {.name = L"AllEmpty",
-         .callback = [] { CHECK(StartsWith(LazyString{}, LazyString{})); }},
-        {.name = L"EmptyInput",
-         .callback =
-             [] { CHECK(!StartsWith(LazyString{}, LazyString{L"foo"})); }},
-        {.name = L"EmptyPrefix",
-         .callback =
-             [] { CHECK(StartsWith(LazyString{L"foo"}, LazyString{})); }},
-        {.name = L"HasPrefix",
-         .callback =
-             [] {
-               CHECK(StartsWith(LazyString{L"foobar"}, LazyString{L"foob"}));
-             }},
-        {.name = L"DifferentPrefix",
-         .callback =
-             [] {
-               CHECK(!StartsWith(LazyString{L"foobar"}, LazyString{L"foab"}));
-             }},
-    });
 
-const bool ends_with_tests_registration = tests::Register(
-    L"LazyString::EndsWith",
-    {
-        {.name = L"AllEmpty",
-         .callback = [] { CHECK(EndsWith(LazyString{}, LazyString{})); }},
-        {.name = L"EmptyInput",
-         .callback =
-             [] { CHECK(!EndsWith(LazyString{}, LazyString{L"foo"})); }},
-        {.name = L"EmptySuffix",
-         .callback = [] { CHECK(EndsWith(LazyString{L"foo"}, LazyString{})); }},
-        {.name = L"HasSuffix",
-         .callback =
-             [] {
-               CHECK(EndsWith(LazyString{L"foobar"}, LazyString{L"obar"}));
-             }},
-        {.name = L"DifferentSuffix",
-         .callback =
-             [] {
-               CHECK(!EndsWith(LazyString{L"foobar"}, LazyString{L"oabar"}));
-             }},
-    });
+TEST_GROUP(LazyString_StartsWith,
+           [](std::pair<LazyString, LazyString> input) {
+             return StartsWith(input.first, input.second);
+           })
+    .Add(L"AllEmpty", {L"", L""}, true)
+    .Add(L"EmptyInput", {L"", L"foo"}, false)
+    .Add(L"EmptyPrefix", {L"foo", L""}, true)
+    .Add(L"HasPrefix", {L"foobar", L"foob"}, true)
+    .Add(L"DifferentPrefix", {L"foobar", L"foab"}, false);
 
-const bool find_first_of_tests_registration = tests::Register(
-    L"LazyString::FindFirstOf",
-    {{.name = L"WithColumnFind",
-      .callback =
-          [] {
-            CHECK_EQ(FindFirstOf(LazyString{L"/home/alejo/edge-clang/edge/src/"
-                                            L"futures::ValueOrError"},
-                                 {L':'}, ColumnNumber{40})
-                         .value(),
-                     ColumnNumber{40});
-          }},
-     {.name = L"WithColumnNotFound", .callback = [] {
-        CHECK(!FindFirstOf(LazyString{L"/home/alejo/edge-clang/edge/src/"
-                                      L"futures::ValueOrError"},
-                           {L':'}, ColumnNumber{41})
-                   .has_value());
-      }}});
+TEST_GROUP(LazyString_EndsWith,
+           [](std::pair<LazyString, LazyString> input) {
+             return EndsWith(input.first, input.second);
+           })
+    .Add(L"AllEmpty", {L"", L""}, true)
+    .Add(L"EmptyInput", {L"", L"foo"}, false)
+    .Add(L"EmptySuffix", {L"foo", L""}, true)
+    .Add(L"HasSuffix", {L"foobar", L"obar"}, true)
+    .Add(L"DifferentSuffix", {L"foobar", L"oabar"}, false);
+
+TEST_GROUP(LazyString_FindFirstOf,
+           ([](std::tuple<LazyString, std::unordered_set<wchar_t>, ColumnNumber>
+                   input) {
+             auto [str, chars, col] = input;
+             return FindFirstOf(str, chars, col);
+           }))
+    .Add(L"Empty", {L"", {L':'}, ColumnNumber{0}}, std::nullopt)
+    .Add(L"None", {L"foobar", {L':'}, ColumnNumber{0}}, std::nullopt)
+    .Add(L"AtResult",
+         {L"/home/alejo/edge-clang/edge/src/futures::ValueOrError",
+          {L':'},
+          ColumnNumber{40}},
+         ColumnNumber{40})
+    .Add(L"PastLast",
+         {L"/home/alejo/edge-clang/edge/src/futures::ValueOrError",
+          {L':'},
+          ColumnNumber{41}},
+         std::nullopt)
+    .Add(L"InBetween", {L"/foo/bar/hey/there", {L'/'}, ColumnNumber{2}},
+         ColumnNumber{4});
 
 }  // namespace
 }  // namespace afc::language::lazy_string
